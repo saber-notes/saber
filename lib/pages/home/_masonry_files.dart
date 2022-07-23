6eb5d66f58dbbb1fb0838d5183dd3e353f451b44
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-07-23 20:48:42 +0100

    recent_notes: make masonry grid its own widget

---
+++ lib/pages/home/_masonry_files.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:saber/data/routes.dart';
+
+class MasonryFiles extends StatelessWidget {
+  const MasonryFiles({
+    Key? key,
+    required this.files,
+  }) : super(key: key);
+
+  final List<String> files;
+
+  @override
+  Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+    return LayoutBuilder(
+      builder: (context, constraints) => MasonryGridView.count(
+        itemCount: files.length,
+        crossAxisCount: constraints.maxWidth ~/ 300 + 1,
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(10),
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return Card(
+            child: InkWell(
+              onTap: () { context.push("${RoutePaths.edit}?path=/example/path/to/note-$index"); },
+              borderRadius: BorderRadius.circular(10),
+              child: SizedBox(
+                height: (100.0 * index) % 500 + 100,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 10),
+                    Text("Note $index"),
+                    const SizedBox(height: 10),
+                    Text(files[index]),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/pages/home/recent_notes.dart
+++ lib/pages/home/recent_notes.dart
@@ -4,6 +4,7 @@ import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:saber/data/routes.dart';
+import 'package:saber/pages/home/_masonry_files.dart';
 
 class RecentPage extends StatelessWidget {
   const RecentPage({Key? key}) : super(key: key);
@@ -16,34 +17,10 @@ class RecentPage extends StatelessWidget {
         toolbarHeight: kToolbarHeight,
         title: const Text("Recent notes"),
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) => MasonryGridView.count(
-          itemCount: 30,
-          crossAxisCount: constraints.maxWidth ~/ 300 + 1,
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(10),
-          physics: const AlwaysScrollableScrollPhysics(),
-          itemBuilder: (context, index) {
-            return Card(
-              child: InkWell(
-                onTap: () { context.push("${RoutePaths.edit}?path=/example/path/to/note-$index"); },
-                borderRadius: BorderRadius.circular(10),
-                child: SizedBox(
-                  height: (100.0 * index) % 500 + 100,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const SizedBox(height: 10),
-                      Text("Note $index"),
-                      const SizedBox(height: 10),
-                      Text("This is an example note"),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
+      body: MasonryFiles(
+        files: [
+          for (int i = 0; i < 30; ++i) "This is example note $i",
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
