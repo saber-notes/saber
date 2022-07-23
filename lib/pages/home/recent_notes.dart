6ace750c0eb108a72d91740fcf327eef85be1bd7
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-07-23 05:22:00 +0100

    recent_notes: open edit page on click

--- lib/pages/home/recent_notes.dart
+++ lib/pages/home/recent_notes.dart
@@ -1,8 +1,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:go_router/go_router.dart';
 
 import 'package:saber/components/navbar/responsive_navbar.dart';
+import 'package:saber/data/routes.dart';
 
 class RecentNotes extends StatelessWidget {
   const RecentNotes({Key? key}) : super(key: key);
@@ -25,16 +27,20 @@ class RecentNotes extends StatelessWidget {
               padding: const EdgeInsets.all(10),
               itemBuilder: (context, index) {
                 return Card(
-                  child: SizedBox(
-                    height: (100.0 * index) % 500 + 100,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const SizedBox(height: 10),
-                        Text("Note $index"),
-                        const SizedBox(height: 10),
-                        Text("This is an example note"),
-                      ],
+                  child: InkWell(
+                    onTap: () { context.go("${RoutePaths.edit}?path=example/path/to/note-$index"); },
+                    borderRadius: BorderRadius.circular(10),
+                    child: SizedBox(
+                      height: (100.0 * index) % 500 + 100,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const SizedBox(height: 10),
+                          Text("Note $index"),
+                          const SizedBox(height: 10),
+                          Text("This is an example note"),
+                        ],
+                      ),
                     ),
                   ),
                 );
