0aa3379428d17572aa0e5efa3d472298b1619b6e
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-07-26 02:07:23 +0100

    canvas: use stroke color

--- lib/components/canvas/_canvas_painter.dart
+++ lib/components/canvas/_canvas_painter.dart
@@ -19,14 +19,15 @@ class CanvasPainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     Paint paint = Paint()
       ..color = Colors.black
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 10.0;
+      ..strokeCap = StrokeCap.round;
     for (final Stroke stroke in strokes) {
+      paint.color = stroke.color;
       Path path = Path();
       path.addPolygon(stroke.getPolygon(), true);
       canvas.drawPath(path, paint);
     }
     if (currentStroke != null) {
+      paint.color = currentStroke!.color;
       Path path = Path();
       path.addPolygon(currentStroke!.getPolygon(), true);
       canvas.drawPath(path, paint);
