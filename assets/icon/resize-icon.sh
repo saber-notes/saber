73392b289d787e234a33e45b1571582da7cec7dd
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-07-27 00:20:57 +0100

    use resized png icons for flatpak

---
+++ assets/icon/resize-icon.sh
@@ -0,0 +1,9 @@
+sizes=('16x16' '24x24' '32x32' '48x48' '64x64' '128x128' '256x256' '512x512')
+
+mkdir -p resized
+
+for size in "${sizes[@]}"
+do :
+    echo "Resizing to $size"
+    convert icon.png -resize ${size} resized/icon-${size}.png
+done
