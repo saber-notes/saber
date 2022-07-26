bfd0cad1b90ac66ff6c387a06054b39970454488
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-07-26 02:39:10 +0100

    editor: relocate all login from canvas
    This is so we can later interface with the toolbar

--- lib/components/canvas/canvas.dart
+++ lib/components/canvas/canvas.dart
@@ -6,100 +6,47 @@ import 'package:flutter/material.dart';
 import 'package:saber/components/canvas/_canvas_painter.dart';
 import '_stroke.dart';
 
-class Canvas extends StatefulWidget {
-  const Canvas({Key? key}) : super(key: key);
+class Canvas extends StatelessWidget {
+  const Canvas({
+    Key? key,
+    required this.undo,
+    required this.redo,
+    required this.strokes,
+    required this.currentStroke,
+    required this.onScaleStart,
+    required this.onScaleUpdate,
+    required this.onScaleEnd,
+    required this.transformationController,
+  }) : super(key: key);
 
   static const double canvasWidth = 1000;
   static const double canvasHeight = canvasWidth * 1.4;
 
-  @override
-  State<Canvas> createState() => _CanvasState();
+  final VoidCallback undo;
+  final VoidCallback redo;
 
-}
+  final ValueChanged<ScaleStartDetails> onScaleStart;
+  final ValueChanged<ScaleUpdateDetails> onScaleUpdate;
+  final ValueChanged<ScaleEndDetails> onScaleEnd;
 
-class _CanvasState extends State<Canvas> {
-  List<Stroke> strokes = [];
-  List<Stroke> strokesRedoStack = [];
-  Stroke? currentStroke;
+  final TransformationController transformationController;
 
-  final TransformationController _transformationController = TransformationController();
-
-  // used to prevent accidentally drawing when pinch zooming
-  int _lastSeenPointerCount = 0;
-  Timer? _lastSeenPointerCountTimer;
-  int get lastSeenPointerCount => _lastSeenPointerCount;
-  set lastSeenPointerCount(int value) {
-    _lastSeenPointerCount = value;
-
-    // reset after 1ms to keep track of the same gesture only
-    _lastSeenPointerCountTimer?.cancel();
-    _lastSeenPointerCountTimer = Timer(const Duration(milliseconds: 1), () {
-      _lastSeenPointerCount = 0;
-    });
-  }
-
-  undo() {
-    if (strokes.isNotEmpty) {
-      setState(() {
-        strokesRedoStack.add(strokes.removeLast());
-      });
-    }
-  }
-
-  redo() {
-    if (strokesRedoStack.isNotEmpty) {
-      setState(() {
-        strokes.add(strokesRedoStack.removeLast());
-      });
-    }
-  }
+  final List<Stroke> strokes;
+  final Stroke? currentStroke;
 
   @override
   Widget build(BuildContext context) {
     return FittedBox(
       child: GestureDetector(
-        onSecondaryTapUp: (TapUpDetails details) {
-          undo();
-        },
-        onTertiaryTapUp: (TapUpDetails details) {
-          redo();
-        },
+        onSecondaryTapUp: (TapUpDetails details) => undo(),
+        onTertiaryTapUp: (TapUpDetails details) => redo(),
         child: InteractiveViewer(
-          transformationController: _transformationController,
+          transformationController: transformationController,
           panEnabled: false,
 
-          onInteractionStart: (ScaleStartDetails details) {
-            if (lastSeenPointerCount >= 2) { // was a zoom gesture, ignore
-              lastSeenPointerCount = lastSeenPointerCount;
-              return;
-            } else if (details.pointerCount >= 2) { // is a zoom gesture, remove accidental stroke
-              if (lastSeenPointerCount == 1) {
-                strokes.removeLast();
-              }
-              _lastSeenPointerCount = details.pointerCount;
-              return;
-            } else { // is a stroke
-              _lastSeenPointerCount = details.pointerCount;
-            }
-
-            currentStroke = Stroke(
-              color: Colors.black,
-              strokeWidth: 2,
-            )..addPoint(_transformationController.toScene(details.localFocalPoint));
-          },
-          onInteractionUpdate: (ScaleUpdateDetails details) {
-            if (currentStroke == null) return;
-            setState(() {
-              currentStroke!.addPoint(_transformationController.toScene(details.localFocalPoint));
-            });
-          },
-          onInteractionEnd: (ScaleEndDetails details) {
-            if (currentStroke == null) return;
-            setState(() {
-              strokes.add(currentStroke!..isComplete = true);
-              currentStroke = null;
-            });
-          },
+          onInteractionStart: onScaleStart,
+          onInteractionUpdate: onScaleUpdate,
+          onInteractionEnd: onScaleEnd,
 
           child: CustomPaint(
             foregroundPainter: CanvasPainter(
--- lib/pages/editor/editor.dart
+++ lib/pages/editor/editor.dart
@@ -1,15 +1,94 @@
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:saber/components/canvas/_stroke.dart';
 import 'package:saber/components/canvas/toolbar.dart';
 import 'package:saber/components/canvas/canvas.dart';
 
-class Editor extends StatelessWidget {
+class Editor extends StatefulWidget {
   const Editor({
     Key? key,
     required this.path,
   }) : super(key: key);
 
   final String? path;
+
+  @override
+  State<Editor> createState() => _EditorState();
+}
+
+class _EditorState extends State<Editor> {
+  List<Stroke> strokes = [];
+  List<Stroke> strokesRedoStack = [];
+  Stroke? currentStroke;
+
+  final TransformationController _transformationController = TransformationController();
+
+  // used to prevent accidentally drawing when pinch zooming
+  int _lastSeenPointerCount = 0;
+  Timer? _lastSeenPointerCountTimer;
+  int get lastSeenPointerCount => _lastSeenPointerCount;
+  set lastSeenPointerCount(int value) {
+    _lastSeenPointerCount = value;
+
+    // reset after 1ms to keep track of the same gesture only
+    _lastSeenPointerCountTimer?.cancel();
+    _lastSeenPointerCountTimer = Timer(const Duration(milliseconds: 1), () {
+      _lastSeenPointerCount = 0;
+    });
+  }
+
+  undo() {
+    if (strokes.isNotEmpty) {
+      setState(() {
+        strokesRedoStack.add(strokes.removeLast());
+      });
+    }
+  }
+
+  redo() {
+    if (strokesRedoStack.isNotEmpty) {
+      setState(() {
+        strokes.add(strokesRedoStack.removeLast());
+      });
+    }
+  }
+
+  onScaleStart(ScaleStartDetails details) {
+    if (lastSeenPointerCount >= 2) { // was a zoom gesture, ignore
+      lastSeenPointerCount = lastSeenPointerCount;
+      return;
+    } else if (details.pointerCount >= 2) { // is a zoom gesture, remove accidental stroke
+      if (lastSeenPointerCount == 1) {
+        strokes.removeLast();
+      }
+      _lastSeenPointerCount = details.pointerCount;
+      return;
+    } else { // is a stroke
+      _lastSeenPointerCount = details.pointerCount;
+    }
+
+    currentStroke = Stroke(
+      color: Colors.black,
+      strokeWidth: 2,
+    )..addPoint(_transformationController.toScene(details.localFocalPoint));
+  }
+  onScaleUpdate(ScaleUpdateDetails details) {
+    if (currentStroke == null) return;
+    setState(() {
+      currentStroke!.addPoint(_transformationController.toScene(details.localFocalPoint));
+    });
+  }
+  onScaleEnd(ScaleEndDetails details) {
+    if (currentStroke == null) return;
+    setState(() {
+      strokes.add(currentStroke!..isComplete = true);
+      currentStroke = null;
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +101,7 @@ class Editor extends StatelessWidget {
             border: InputBorder.none,
           ),
           controller: TextEditingController(
-            text: path ?? "New note",
+            text: widget.path ?? "New note",
           ),
           onChanged: (text) {
             // todo: update note name
@@ -31,9 +110,18 @@ class Editor extends StatelessWidget {
         ),
       ),
       body: Column(
-        children: const [
+        children: [
           Toolbar(),
-          Expanded(child: Canvas()),
+          Expanded(child: Canvas(
+            undo: undo,
+            redo: redo,
+            strokes: strokes,
+            currentStroke: currentStroke,
+            onScaleStart: onScaleStart,
+            onScaleUpdate: onScaleUpdate,
+            onScaleEnd: onScaleEnd,
+            transformationController: _transformationController,
+          )),
         ],
       )
     );
