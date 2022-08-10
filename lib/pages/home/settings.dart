eff2e78658dd45b69bd37ab41515b738ab7f68d8
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-08-10 01:37:50 +0100

    settings: add link to privacy policy

--- lib/pages/home/settings.dart
+++ lib/pages/home/settings.dart
@@ -1,8 +1,11 @@
 
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
+
+  static final Uri privacyPolicyUrl = Uri.parse('https://github.com/adil192/saber/blob/main/privacy_policy.md');
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +15,16 @@ class SettingsPage extends StatelessWidget {
         toolbarHeight: kToolbarHeight,
         title: const Text("Settings"),
       ),
-      body: const Center(
-        child: Text("Settings"),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            launchUrl(
+              privacyPolicyUrl,
+              mode: LaunchMode.externalApplication,
+            );
+          },
+          child: Text("Tap here to view the privacy policy"),
+        ),
       ),
     );
   }
