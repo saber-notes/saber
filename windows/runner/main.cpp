84b2253a14ff599ea90edfc78a9c1a8b6ed07b0d
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-08-16 00:11:00 +0100

    windows: use capital S for Saber

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"saber", origin, size)) {
+  if (!window.CreateAndShow(L"Saber", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
