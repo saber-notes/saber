b49b431f071ee9f6225790461bbb3e0edbca1052
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-09-01 12:13:30 +0100

    pubspec: flutter pub upgrade

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,14 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <dynamic_color/dynamic_color_plugin.h>
 #include <gtk_theme_fl/gtk_theme_fl_plugin.h>
 #include <url_launcher_linux/url_launcher_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) dynamic_color_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "DynamicColorPlugin");
+  dynamic_color_plugin_register_with_registrar(dynamic_color_registrar);
   g_autoptr(FlPluginRegistrar) gtk_theme_fl_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "GtkThemeFlPlugin");
   gtk_theme_fl_plugin_register_with_registrar(gtk_theme_fl_registrar);
--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  dynamic_color
   gtk_theme_fl
   url_launcher_linux
 )
