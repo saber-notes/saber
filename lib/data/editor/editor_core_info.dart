3905e5f4459e4676742d4aacc8d7627f0736865b
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-09-18 05:29:57 +0100

    editor: store width,height in note file

--- lib/data/editor/editor_core_info.dart
+++ lib/data/editor/editor_core_info.dart
@@ -8,21 +8,32 @@ import 'package:saber/data/file_manager/file_manager.dart';
 import 'package:saber/pages/editor/editor.dart';
 
 class EditorCoreInfo {
-  static const int fileVersion = 1;
+  static const int fileVersion = 2;
+
+  static const double defaultWidth = 1000;
+  static const double defaultHeight = defaultWidth * 1.4;
 
   final List<Stroke> strokes;
   Color? backgroundColor;
+  final double width;
+  final double height;
 
   EditorCoreInfo({
     List<Stroke>? strokes,
     this.backgroundColor,
+    this.width = defaultWidth,
+    this.height = defaultHeight,
   }): strokes = strokes ?? [];
 
   EditorCoreInfo.fromJson(Map<String, dynamic> json):
         strokes = _parseStrokesJson(json["s"] as List),
-        backgroundColor = json["b"] != null ? Color(json["b"] as int) : null;
+        backgroundColor = json["b"] != null ? Color(json["b"] as int) : null,
+        width = json["w"] ?? defaultWidth,
+        height = json["h"] ?? defaultHeight;
   EditorCoreInfo.fromOldJson(List<dynamic> json):
-        strokes = _parseStrokesJson(json);
+        strokes = _parseStrokesJson(json),
+        width = defaultWidth,
+        height = defaultHeight;
 
   static List<Stroke> _parseStrokesJson(List<dynamic> strokes) => strokes
       .map((dynamic stroke) => Stroke.fromJson(stroke as Map<String, dynamic>))
@@ -52,14 +63,21 @@ class EditorCoreInfo {
     'v': fileVersion,
     's': strokes,
     'b': backgroundColor?.value,
+    'w': width,
+    'h': height,
   };
 
   EditorCoreInfo copyWith({
     List<Stroke>? strokes,
+    Color? backgroundColor,
+    double? width,
+    double? height,
   }) {
     return EditorCoreInfo(
       strokes: strokes ?? this.strokes,
-      backgroundColor: backgroundColor,
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      width: width ?? this.width,
+      height: height ?? this.height,
     );
   }
 }
