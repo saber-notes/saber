2e221c6b009b1eebd580cbc2b8a05b3fdfa5dc57
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-10-14 10:54:16 +0100

    Generate Chinese strings
    `flutter pub run slang`

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -1,9 +1,9 @@
 /// Generated file. Do not edit.
 ///
-/// Locales: 1
-/// Strings: 56
+/// Locales: 2
+/// Strings: 112 (56 per locale)
 ///
-/// Built on 2022-10-09 at 19:45 UTC
+/// Built on 2022-10-14 at 09:53 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
@@ -22,7 +22,8 @@ const AppLocale _baseLocale = AppLocale.en;
 /// - Locale locale = AppLocale.en.flutterLocale // get flutter locale from enum
 /// - if (LocaleSettings.currentLocale == AppLocale.en) // locale check
 enum AppLocale with BaseAppLocale<AppLocale, _StringsEn> {
-	en(languageCode: 'en', build: _StringsEn.build);
+	en(languageCode: 'en', build: _StringsEn.build),
+	zh(languageCode: 'zh', build: _StringsZh.build);
 
 	const AppLocale({required this.languageCode, this.scriptCode, this.countryCode, required this.build}); // ignore: unused_element
 
@@ -353,6 +354,237 @@ class _StringsEditorToolbarEn {
 	String get exportAs => 'Export as:';
 }
 
+// Path: <root>
+class _StringsZh implements _StringsEn {
+
+	/// You can call this constructor and build your own translation instance of this locale.
+	/// Constructing via the enum [AppLocale.build] is preferred.
+	_StringsZh.build({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver})
+		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
+		  $meta = TranslationMetadata(
+		    locale: AppLocale.zh,
+		    overrides: overrides ?? {},
+		    cardinalResolver: cardinalResolver,
+		    ordinalResolver: ordinalResolver,
+		  ) {
+		$meta.setFlatMapFunction(_flatMapFunction);
+	}
+
+	/// Metadata for the translations of <zh>.
+	@override final TranslationMetadata<AppLocale, _StringsEn> $meta;
+
+	/// Access flat map
+	@override dynamic operator[](String key) => $meta.getTranslation(key);
+
+	@override late final _StringsZh _root = this; // ignore: unused_field
+
+	// Translations
+	@override late final _StringsHomeZh home = _StringsHomeZh._(_root);
+	@override late final _StringsSettingsZh settings = _StringsSettingsZh._(_root);
+	@override late final _StringsLoginZh login = _StringsLoginZh._(_root);
+	@override late final _StringsAppInfoZh appInfo = _StringsAppInfoZh._(_root);
+	@override late final _StringsEditorZh editor = _StringsEditorZh._(_root);
+}
+
+// Path: home
+class _StringsHomeZh implements _StringsHomeEn {
+	_StringsHomeZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override late final _StringsHomeTabsZh tabs = _StringsHomeTabsZh._(_root);
+	@override late final _StringsHomeTitlesZh titles = _StringsHomeTitlesZh._(_root);
+	@override late final _StringsHomeTooltipsZh tooltips = _StringsHomeTooltipsZh._(_root);
+	@override String get backFolder => '(返回)';
+	@override String get welcome => '欢迎使用 Saber';
+	@override String get noFiles => '未找到文件';
+	@override String get createNewNote => '点击 + 按钮新建一个笔记';
+}
+
+// Path: settings
+class _StringsSettingsZh implements _StringsSettingsEn {
+	_StringsSettingsZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override late final _StringsSettingsPrefLabelsZh prefLabels = _StringsSettingsPrefLabelsZh._(_root);
+	@override late final _StringsSettingsThemeModesZh themeModes = _StringsSettingsThemeModesZh._(_root);
+}
+
+// Path: login
+class _StringsLoginZh implements _StringsLoginEn {
+	_StringsLoginZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get title => 'Login';
+	@override late final _StringsLoginFeedbacksZh feedbacks = _StringsLoginFeedbacksZh._(_root);
+	@override late final _StringsLoginFormZh form = _StringsLoginFormZh._(_root);
+	@override TextSpan signup({required InlineSpanBuilder linkToSignup}) => TextSpan(children: [
+		const TextSpan(text: '还没有帐户？ '),
+		linkToSignup('立即注册'),
+		const TextSpan(text: '！'),
+	]);
+	@override late final _StringsLoginStatusZh status = _StringsLoginStatusZh._(_root);
+}
+
+// Path: appInfo
+class _StringsAppInfoZh implements _StringsAppInfoEn {
+	_StringsAppInfoZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get licenseNotice => 'Saber  版权所有 (C) 2022  Adil Hanney\nThis program comes with absolutely no warranty. This is free software, and you are welcome to redistribute it under certain conditions.';
+	@override String get debug => 'DEBUG';
+	@override String get showMoreInfo => '显示更多信息';
+	@override String get showLessInfo => '显示更少信息';
+	@override String get licenseButton => '点击此处查看更多许可证信息';
+	@override String get privacyPolicyButton => '点击此处查看隐私政策';
+}
+
+// Path: editor
+class _StringsEditorZh implements _StringsEditorEn {
+	_StringsEditorZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override late final _StringsEditorToolbarZh toolbar = _StringsEditorToolbarZh._(_root);
+}
+
+// Path: home.tabs
+class _StringsHomeTabsZh implements _StringsHomeTabsEn {
+	_StringsHomeTabsZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get home => '主页';
+	@override String get browse => '浏览';
+	@override String get whiteboard => '白板';
+	@override String get settings => '设置';
+}
+
+// Path: home.titles
+class _StringsHomeTitlesZh implements _StringsHomeTitlesEn {
+	_StringsHomeTitlesZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get home => '最近笔记';
+	@override String get browse => '浏览';
+	@override String get whiteboard => '白板';
+	@override String get settings => '设置';
+}
+
+// Path: home.tooltips
+class _StringsHomeTooltipsZh implements _StringsHomeTooltipsEn {
+	_StringsHomeTooltipsZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get showHideActions => '显示/隐藏操作';
+	@override String get newNote => '新建笔记';
+	@override String get showUpdateDialog => '显示更新对话框';
+}
+
+// Path: settings.prefLabels
+class _StringsSettingsPrefLabelsZh implements _StringsSettingsPrefLabelsEn {
+	_StringsSettingsPrefLabelsZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get appTheme => '应用主题';
+	@override String get shouldCheckForUpdates => '自动检查 Saber 更新';
+	@override String get editorToolbarOnBottom => '将编辑工具栏与底部对齐';
+	@override String get editorAutoInvert => '在深色模式下使用反色笔记背景';
+	@override String get preferGreyscale => '使用灰度颜色';
+}
+
+// Path: settings.themeModes
+class _StringsSettingsThemeModesZh implements _StringsSettingsThemeModesEn {
+	_StringsSettingsThemeModesZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get system => '系统';
+	@override String get light => '浅色';
+	@override String get dark => '深色';
+}
+
+// Path: login.feedbacks
+class _StringsLoginFeedbacksZh implements _StringsLoginFeedbacksEn {
+	_StringsLoginFeedbacksZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get checkUsername => '请仔细检查您的用户名或电子邮件。';
+	@override String get enterNcPassword => '请输入您的 Nextcloud 密码。';
+	@override String get enterEncPassword => '请输入您的加密密码。';
+	@override String get checkUrl => '请输入有效链接。';
+	@override String get loginFailed => '登录失败，请检查您的详细信息和网络连接。';
+	@override String get loginSuccess => '登录成功！我们正在设置中，请稍候……';
+}
+
+// Path: login.form
+class _StringsLoginFormZh implements _StringsLoginFormEn {
+	_StringsLoginFormZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get useCustomServer => '我想要使用自定义 Nextcloud 服务器';
+	@override String get customServerUrl => '自定义服务器链接';
+	@override String get username => '用户名或邮箱';
+	@override String get ncPassword => 'Nextcloud 密码';
+	@override String get encPassword => '加密密码';
+	@override TextSpan agreeToPrivacyPolicy({required InlineSpanBuilder linkToPrivacyPolicy}) => TextSpan(children: [
+		const TextSpan(text: '登录即表示您同意 '),
+		linkToPrivacyPolicy('隐私政策'),
+		const TextSpan(text: '。'),
+	]);
+	@override String get login => 'Log in';
+}
+
+// Path: login.status
+class _StringsLoginStatusZh implements _StringsLoginStatusEn {
+	_StringsLoginStatusZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get loggedIn => '已使用 Nextcloud 登录';
+	@override String get loggedOut => '已注销';
+	@override String get tapToLogin => '点击以使用 Nextcloud 登录';
+}
+
+// Path: editor.toolbar
+class _StringsEditorToolbarZh implements _StringsEditorToolbarEn {
+	_StringsEditorToolbarZh._(this._root);
+
+	@override final _StringsZh _root; // ignore: unused_field
+
+	// Translations
+	@override String get fountainPen => '钢笔';
+	@override String get toggleColors => '切换颜色 (Ctrl C)';
+	@override String get toggleEraser => '切换橡皮擦 (Ctrl E)';
+	@override String get photo => '照片';
+	@override String get toggleFingerDrawing => '切换手写 (Ctrl F)';
+	@override String get undo => '撤销';
+	@override String get redo => '重做';
+	@override String get export => '导出 (Ctrl Shift S)';
+	@override String get exportAs => '导出为：';
+}
+
 /// Flat map(s) containing all translations.
 /// Only for edge cases! For simple maps, use the map function of this library.
 
@@ -423,6 +655,78 @@ extension on _StringsEn {
 			case 'editor.toolbar.redo': return 'Redo';
 			case 'editor.toolbar.export': return 'Export (Ctrl Shift S)';
 			case 'editor.toolbar.exportAs': return 'Export as:';
+			default: return null;
+		}
+	}
+}
+
+extension on _StringsZh {
+	dynamic _flatMapFunction(String path) {
+		switch (path) {
+			case 'home.tabs.home': return '主页';
+			case 'home.tabs.browse': return '浏览';
+			case 'home.tabs.whiteboard': return '白板';
+			case 'home.tabs.settings': return '设置';
+			case 'home.titles.home': return '最近笔记';
+			case 'home.titles.browse': return '浏览';
+			case 'home.titles.whiteboard': return '白板';
+			case 'home.titles.settings': return '设置';
+			case 'home.tooltips.showHideActions': return '显示/隐藏操作';
+			case 'home.tooltips.newNote': return '新建笔记';
+			case 'home.tooltips.showUpdateDialog': return '显示更新对话框';
+			case 'home.backFolder': return '(返回)';
+			case 'home.welcome': return '欢迎使用 Saber';
+			case 'home.noFiles': return '未找到文件';
+			case 'home.createNewNote': return '点击 + 按钮新建一个笔记';
+			case 'settings.prefLabels.appTheme': return '应用主题';
+			case 'settings.prefLabels.shouldCheckForUpdates': return '自动检查 Saber 更新';
+			case 'settings.prefLabels.editorToolbarOnBottom': return '将编辑工具栏与底部对齐';
+			case 'settings.prefLabels.editorAutoInvert': return '在深色模式下使用反色笔记背景';
+			case 'settings.prefLabels.preferGreyscale': return '使用灰度颜色';
+			case 'settings.themeModes.system': return '系统';
+			case 'settings.themeModes.light': return '浅色';
+			case 'settings.themeModes.dark': return '深色';
+			case 'login.title': return 'Login';
+			case 'login.feedbacks.checkUsername': return '请仔细检查您的用户名或电子邮件。';
+			case 'login.feedbacks.enterNcPassword': return '请输入您的 Nextcloud 密码。';
+			case 'login.feedbacks.enterEncPassword': return '请输入您的加密密码。';
+			case 'login.feedbacks.checkUrl': return '请输入有效链接。';
+			case 'login.feedbacks.loginFailed': return '登录失败，请检查您的详细信息和网络连接。';
+			case 'login.feedbacks.loginSuccess': return '登录成功！我们正在设置中，请稍候……';
+			case 'login.form.useCustomServer': return '我想要使用自定义 Nextcloud 服务器';
+			case 'login.form.customServerUrl': return '自定义服务器链接';
+			case 'login.form.username': return '用户名或邮箱';
+			case 'login.form.ncPassword': return 'Nextcloud 密码';
+			case 'login.form.encPassword': return '加密密码';
+			case 'login.form.agreeToPrivacyPolicy': return ({required InlineSpanBuilder linkToPrivacyPolicy}) => TextSpan(children: [
+				const TextSpan(text: '登录即表示您同意 '),
+				linkToPrivacyPolicy('隐私政策'),
+				const TextSpan(text: '。'),
+			]);
+			case 'login.form.login': return 'Log in';
+			case 'login.signup': return ({required InlineSpanBuilder linkToSignup}) => TextSpan(children: [
+				const TextSpan(text: '还没有帐户？ '),
+				linkToSignup('立即注册'),
+				const TextSpan(text: '！'),
+			]);
+			case 'login.status.loggedIn': return '已使用 Nextcloud 登录';
+			case 'login.status.loggedOut': return '已注销';
+			case 'login.status.tapToLogin': return '点击以使用 Nextcloud 登录';
+			case 'appInfo.licenseNotice': return 'Saber  版权所有 (C) 2022  Adil Hanney\nThis program comes with absolutely no warranty. This is free software, and you are welcome to redistribute it under certain conditions.';
+			case 'appInfo.debug': return 'DEBUG';
+			case 'appInfo.showMoreInfo': return '显示更多信息';
+			case 'appInfo.showLessInfo': return '显示更少信息';
+			case 'appInfo.licenseButton': return '点击此处查看更多许可证信息';
+			case 'appInfo.privacyPolicyButton': return '点击此处查看隐私政策';
+			case 'editor.toolbar.fountainPen': return '钢笔';
+			case 'editor.toolbar.toggleColors': return '切换颜色 (Ctrl C)';
+			case 'editor.toolbar.toggleEraser': return '切换橡皮擦 (Ctrl E)';
+			case 'editor.toolbar.photo': return '照片';
+			case 'editor.toolbar.toggleFingerDrawing': return '切换手写 (Ctrl F)';
+			case 'editor.toolbar.undo': return '撤销';
+			case 'editor.toolbar.redo': return '重做';
+			case 'editor.toolbar.export': return '导出 (Ctrl Shift S)';
+			case 'editor.toolbar.exportAs': return '导出为：';
 			default: return null;
 		}
 	}
