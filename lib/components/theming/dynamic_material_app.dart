d2c644f1851c155fa1c2c51aa3f5c70ede698fca
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-10-19 20:16:01 +0100

    theming: use custom font on old macOS versions
    #26

--- lib/components/theming/dynamic_material_app.dart
+++ lib/components/theming/dynamic_material_app.dart
@@ -1,9 +1,11 @@
 
+import 'dart:io';
 
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:saber/data/prefs.dart';
 import 'package:saber/i18n/strings.g.dart';
 
@@ -24,15 +26,33 @@ class DynamicMaterialApp extends StatefulWidget {
 }
 
 class _DynamicMaterialAppState extends State<DynamicMaterialApp> {
+  bool useCustomFont = false;
+
   @override
   void initState() {
     Prefs.appTheme.addListener(onChanged);
     Prefs.accentColor.addListener(onChanged);
+    decideOnFont();
     super.initState();
   }
 
   void onChanged() {
     setState(() { });
+  }
+
+  /// We need to use a custom font if macOS < 10.13,
+  /// see https://github.com/adil192/saber/issues/26
+  void decideOnFont() {
+    if (!Platform.isMacOS) return;
+
+    final RegExp numberRegex = RegExp(r'\d+\.\d+'); // e.g. 10.13 or 12.5
+    final RegExpMatch? osVersionMatch = numberRegex.firstMatch(Platform.operatingSystemVersion);
+    if (osVersionMatch == null) return;
+
+    final double osVersion = double.tryParse(osVersionMatch[0] ?? "0") ?? 0;
+    if (osVersion >= 10.13) return;
+
+    useCustomFont = true;
   }
 
   @override
@@ -77,10 +97,16 @@ class _DynamicMaterialAppState extends State<DynamicMaterialApp> {
           theme: ThemeData(
             useMaterial3: true,
             colorScheme: lightColorScheme,
+            textTheme: useCustomFont ? GoogleFonts.robotoTextTheme(
+              ThemeData(brightness: Brightness.light).textTheme,
+            ) : null,
           ),
           darkTheme: ThemeData(
             useMaterial3: true,
             colorScheme: darkColorScheme,
+            textTheme: useCustomFont ? GoogleFonts.robotoTextTheme(
+              ThemeData(brightness: Brightness.dark).textTheme,
+            ) : null,
           ),
           themeMode: ThemeMode.values[Prefs.appTheme.value],
 
