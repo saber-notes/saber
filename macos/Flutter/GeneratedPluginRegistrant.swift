36524d8f85bc60a64c4ae450d1c21cf235168e4e
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-10-22 16:35:37 +0100

    pubspec: flutter pub upgrade --major-versions

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import dynamic_color
-import package_info_plus_macos
+import package_info_plus
 import path_provider_macos
-import share_plus_macos
+import share_plus
 import shared_preferences_macos
 import url_launcher_macos
 
