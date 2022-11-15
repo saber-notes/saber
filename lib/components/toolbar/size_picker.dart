1aefecc95303ae8216b36b5ccda89e9671ca641c
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-11-15 21:01:47 +0000

    penModal: show resizing mouse cursor

--- lib/components/toolbar/size_picker.dart
+++ lib/components/toolbar/size_picker.dart
@@ -69,59 +69,62 @@ class _SizePickerState extends State<SizePicker> {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    return GestureDetector(
-      behavior: HitTestBehavior.opaque,
-      onPanStart: (DragStartDetails details) {
-        startingOffset = details.globalPosition;
-        startingValue = widget.currentTool.strokeProperties.size;
-      },
-      onPanUpdate: (DragUpdateDetails details) {
-        onDrag(details.globalPosition);
-      },
-      onPanEnd: (DragEndDetails details) {
-        startingOffset = null;
-      },
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          children: [
-            Text(t.editor.penOptions.size),
-            Container(
-              width: widget.max,
-              height: widget.max,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: colorScheme.onBackground,
-                  width: 1,
-                ),
-                shape: BoxShape.circle,
-              ),
-              child: Center(
-                child: Container(
-                  width: Pen.currentPen.strokeProperties.size,
-                  height: Pen.currentPen.strokeProperties.size,
-                  decoration: BoxDecoration(
+    return MouseRegion(
+      cursor: SystemMouseCursors.resizeLeftRight,
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onPanStart: (DragStartDetails details) {
+          startingOffset = details.globalPosition;
+          startingValue = widget.currentTool.strokeProperties.size;
+        },
+        onPanUpdate: (DragUpdateDetails details) {
+          onDrag(details.globalPosition);
+        },
+        onPanEnd: (DragEndDetails details) {
+          startingOffset = null;
+        },
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              Text(t.editor.penOptions.size),
+              Container(
+                width: widget.max,
+                height: widget.max,
+                decoration: BoxDecoration(
+                  border: Border.all(
                     color: colorScheme.onBackground,
-                    shape: BoxShape.circle,
+                    width: 1,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+                child: Center(
+                  child: Container(
+                    width: Pen.currentPen.strokeProperties.size,
+                    height: Pen.currentPen.strokeProperties.size,
+                    decoration: BoxDecoration(
+                      color: colorScheme.onBackground,
+                      shape: BoxShape.circle,
+                    ),
                   ),
                 ),
               ),
-            ),
-            ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: widget.max * 2),
-              child: TextField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                  isDense: true,
-                  border: InputBorder.none,
+              ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: widget.max * 2),
+                child: TextField(
+                  controller: _controller,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    border: InputBorder.none,
+                  ),
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-                style: const TextStyle(
-                  fontSize: 14,
-                ),
-                textAlign: TextAlign.center,
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
