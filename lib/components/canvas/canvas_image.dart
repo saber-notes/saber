bbddaf5a8deff52637880b50af9da1fecc0c5568
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-11-19 00:23:22 +0000

    editorImage: show resize dots when active
    Co-Authored-By: JoshChadwick <89281441+JoshChadwick@users.noreply.github.com>

--- lib/components/canvas/canvas_image.dart
+++ lib/components/canvas/canvas_image.dart
@@ -40,35 +40,47 @@ class _CanvasImageState extends State<CanvasImage> {
       top: widget.image.dstRect.top,
       width: widget.image.dstRect.width,
       height: widget.image.dstRect.height,
-      child: GestureDetector(
-        onTap: () {
-          setState(() {
-            if (!active) {
-              CanvasImage.activeListener.notifyListeners();
-              active = true;
-            } else {
-              active = false;
-            }
-          });
-        },
-        onPanUpdate: active ? (details) {
-          setState(() {
-            widget.image.dstRect = widget.image.dstRect.translate(
-              details.delta.dx,
-              details.delta.dy,
-            );
-          });
-        } : null,
-        child: SizedOverflowBox(
-          size: widget.image.srcRect.size,
-          child: Transform.translate(
-            offset: -widget.image.srcRect.topLeft,
-            child: Image.memory(
-              widget.image.bytes,
-              fit: BoxFit.none,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                if (!active) {
+                  CanvasImage.activeListener.notifyListeners();
+                  active = true;
+                } else {
+                  active = false;
+                }
+              });
+            },
+            onPanUpdate: active ? (details) {
+              setState(() {
+                widget.image.dstRect = widget.image.dstRect.translate(
+                  details.delta.dx,
+                  details.delta.dy,
+                );
+              });
+            } : null,
+            child: SizedOverflowBox(
+              size: widget.image.srcRect.size,
+              child: Transform.translate(
+                offset: -widget.image.srcRect.topLeft,
+                child: Image.memory(
+                  widget.image.bytes,
+                  fit: BoxFit.none,
+                ),
+              ),
             ),
           ),
-        ),
+          if (active)
+            for (double x = -10; x <= 10; x += 10 * 2)
+              for (double y = -10; y <= 10; y += 10 * 2)
+                CanvasImageResizeHandle(
+                  position: Offset(x, y),
+                  image: widget.image,
+                ),
+        ],
       ),
     );
   }
@@ -77,5 +89,41 @@ class _CanvasImageState extends State<CanvasImage> {
   void dispose() {
     CanvasImage.activeListener.removeListener(disableActive);
     super.dispose();
+  }
+}
+
+class CanvasImageResizeHandle extends StatelessWidget {
+  const CanvasImageResizeHandle({
+    super.key,
+    required this.position,
+    required this.image,
+  });
+
+  final Offset position;
+  final EditorImage image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: position.dx < 0 ? position.dx : null,
+      right: position.dx >= 0 ? -position.dx : null,
+      top: position.dy < 0 ? position.dy : null,
+      bottom: position.dy >= 0 ? -position.dy : null,
+      child: GestureDetector(
+        // todo: resize on pan
+        child: Container(
+          width: 20,
+          height: 20,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.black,
+              width: 2,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
