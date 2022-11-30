5c3b9b69e83d2fdbe6b7a85871415d691336297e
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-11-30 13:51:23 +0000

    editorMenu: add padding

--- lib/components/toolbar/editor_bottom_sheet.dart
+++ lib/components/toolbar/editor_bottom_sheet.dart
@@ -30,118 +30,115 @@ class _EditorBottomSheetState extends State<EditorBottomSheet> {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Flexible(
-          child: IntrinsicHeight(
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(
-                maxWidth: 500,
-              ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const SizedBox(height: 16),
-                  Row(
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+      child: IntrinsicHeight(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 500,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        style: !widget.coreInfo.isEmpty ? TextButton.styleFrom(
+                          backgroundColor: colorScheme.primary,
+                          foregroundColor: colorScheme.onPrimary,
+                        ) : null,
+                        onPressed: !widget.coreInfo.isEmpty ? () {
+                          widget.clearPage();
+                          Navigator.pop(context);
+                        } : null,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.delete),
+                            const SizedBox(width: 8),
+                            Text(t.editor.menu.clearPage),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: ElevatedButton(
+                        style: !widget.coreInfo.isEmpty ? TextButton.styleFrom(
+                          backgroundColor: colorScheme.primary,
+                          foregroundColor: colorScheme.onPrimary,
+                        ) : null,
+                        onPressed: !widget.coreInfo.isEmpty ? () {
+                          widget.clearAllPages();
+                          Navigator.pop(context);
+                        } : null,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(Icons.delete_sweep),
+                            const SizedBox(width: 8),
+                            Text(t.editor.menu.clearAllPages),
+                          ],
+                        )
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
                     children: [
-                      Expanded(
-                        child: ElevatedButton(
-                          style: !widget.coreInfo.isEmpty ? TextButton.styleFrom(
-                            backgroundColor: colorScheme.primary,
-                            foregroundColor: colorScheme.onPrimary,
-                          ) : null,
-                          onPressed: !widget.coreInfo.isEmpty ? () {
-                            widget.clearPage();
-                            Navigator.pop(context);
-                          } : null,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(Icons.delete),
-                              const SizedBox(width: 8),
-                              Text(t.editor.menu.clearPage),
-                            ],
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: ElevatedButton(
-                          style: !widget.coreInfo.isEmpty ? TextButton.styleFrom(
-                            backgroundColor: colorScheme.primary,
-                            foregroundColor: colorScheme.onPrimary,
-                          ) : null,
-                          onPressed: !widget.coreInfo.isEmpty ? () {
-                            widget.clearAllPages();
-                            Navigator.pop(context);
-                          } : null,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Icon(Icons.delete_sweep),
-                              const SizedBox(width: 8),
-                              Text(t.editor.menu.clearAllPages),
-                            ],
-                          )
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 16),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        for (final String backgroundPattern in CanvasBackgroundPatterns.all) ...[
-                          InkWell(
-                            borderRadius: BorderRadius.circular(8),
-                            onTap: () => setState(() {
-                              widget.setBackgroundPattern(backgroundPattern);
-                            }),
-                            child: Container(
-                              width: 150,
-                              decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: colorScheme.primary.withOpacity(0.5).withSaturation(widget.coreInfo.backgroundPattern == backgroundPattern ? 1 : 0),
-                                  width: 2,
-                                ),
-                                borderRadius: BorderRadius.circular(8),
+                      for (final String backgroundPattern in CanvasBackgroundPatterns.all) ...[
+                        InkWell(
+                          borderRadius: BorderRadius.circular(8),
+                          onTap: () => setState(() {
+                            widget.setBackgroundPattern(backgroundPattern);
+                          }),
+                          child: Container(
+                            width: 150,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: colorScheme.primary.withOpacity(0.5).withSaturation(widget.coreInfo.backgroundPattern == backgroundPattern ? 1 : 0),
+                                width: 2,
                               ),
-                              clipBehavior: Clip.antiAlias,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(8),
-                                child: FittedBox(
-                                  child: SizedBox(
-                                    width: widget.coreInfo.width / 5,
-                                    height: widget.coreInfo.height / 5,
-                                    child: CustomPaint(
-                                      painter: CanvasBackgroundPainter(
-                                        invert: widget.invert,
-                                        backgroundColor: widget.coreInfo.backgroundColor ?? InnerCanvas.defaultBackgroundColor,
-                                        backgroundPattern: backgroundPattern,
-                                        primaryColor: colorScheme.primary.withSaturation(widget.coreInfo.backgroundPattern == backgroundPattern ? 1 : 0),
-                                        secondaryColor: colorScheme.secondary.withSaturation(widget.coreInfo.backgroundPattern == backgroundPattern ? 1 : 0),
-                                        preview: true,
-                                      ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            clipBehavior: Clip.antiAlias,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8),
+                              child: FittedBox(
+                                child: SizedBox(
+                                  width: widget.coreInfo.width / 5,
+                                  height: widget.coreInfo.height / 5,
+                                  child: CustomPaint(
+                                    painter: CanvasBackgroundPainter(
+                                      invert: widget.invert,
+                                      backgroundColor: widget.coreInfo.backgroundColor ?? InnerCanvas.defaultBackgroundColor,
+                                      backgroundPattern: backgroundPattern,
+                                      primaryColor: colorScheme.primary.withSaturation(widget.coreInfo.backgroundPattern == backgroundPattern ? 1 : 0),
+                                      secondaryColor: colorScheme.secondary.withSaturation(widget.coreInfo.backgroundPattern == backgroundPattern ? 1 : 0),
+                                      preview: true,
                                     ),
                                   ),
                                 ),
                               ),
                             ),
                           ),
-                          const SizedBox(width: 8),
-                        ],
+                        ),
+                        const SizedBox(width: 8),
                       ],
-                    ),
+                    ],
                   ),
-                  const SizedBox(height: 16),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
