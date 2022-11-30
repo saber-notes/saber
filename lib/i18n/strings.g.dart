bb99f14148422debc9be94068fc0f4f62028b07f
Josue Fischer <josuefischercraft@gmail.com>
2022-11-30 05:19:58 -0500

    i18n: Update Spanish (#97)
    * Update missing Spanish translations
    
    * i18n: apply strings

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -1,9 +1,9 @@
 /// Generated file. Do not edit.
 ///
 /// Locales: 6
-/// Strings: 452 (75 per locale)
+/// Strings: 453 (75 per locale)
 ///
-/// Built on 2022-11-30 at 10:16 UTC
+/// Built on 2022-11-30 at 10:17 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
@@ -910,6 +910,7 @@ class _StringsEditorEs extends _StringsEditorEn {
 	@override late final _StringsEditorPensEs pens = _StringsEditorPensEs._(_root);
 	@override late final _StringsEditorPenOptionsEs penOptions = _StringsEditorPenOptionsEs._(_root);
 	@override late final _StringsEditorImageOptionsEs imageOptions = _StringsEditorImageOptionsEs._(_root);
+	@override late final _StringsEditorMenuEs menu = _StringsEditorMenuEs._(_root);
 }
 
 // Path: home.tabs
@@ -1086,6 +1087,7 @@ class _StringsEditorPensEs extends _StringsEditorPensEn {
 
 	// Translations
 	@override String get fountainPen => 'Lápiz';
+	@override String get ballpointPen => 'Bolígrafo';
 	@override String get highlighter => 'Resaltador';
 }
 
@@ -1109,6 +1111,17 @@ class _StringsEditorImageOptionsEs extends _StringsEditorImageOptionsEn {
 	@override String get title => 'Opciones de imagen';
 	@override String get invertible => 'Invertible';
 	@override String get delete => 'Eliminar';
+}
+
+// Path: editor.menu
+class _StringsEditorMenuEs extends _StringsEditorMenuEn {
+	_StringsEditorMenuEs._(_StringsEs root) : this._root = root, super._(root);
+
+	@override final _StringsEs _root; // ignore: unused_field
+
+	// Translations
+	@override String get clearPage => 'Borrar página';
+	@override String get clearAllPages => 'Borrar todas las páginas';
 }
 
 // Path: <root>
@@ -2341,11 +2354,14 @@ extension on _StringsEs {
 			case 'editor.toolbar.export': return 'Exportar (Ctrl Shift S)';
 			case 'editor.toolbar.exportAs': return 'Exportar cómo:';
 			case 'editor.pens.fountainPen': return 'Lápiz';
+			case 'editor.pens.ballpointPen': return 'Bolígrafo';
 			case 'editor.pens.highlighter': return 'Resaltador';
 			case 'editor.penOptions.size': return 'Tamaño';
 			case 'editor.imageOptions.title': return 'Opciones de imagen';
 			case 'editor.imageOptions.invertible': return 'Invertible';
 			case 'editor.imageOptions.delete': return 'Eliminar';
+			case 'editor.menu.clearPage': return 'Borrar página';
+			case 'editor.menu.clearAllPages': return 'Borrar todas las páginas';
 			default: return null;
 		}
 	}
