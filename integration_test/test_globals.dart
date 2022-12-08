fe6c577cc3dccb704e7c2050d608f8a6793a2ca3
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-12-08 21:17:48 +0000

    test: test encrypting/decrypting a filename
    #118

--- integration_test/test_globals.dart
+++ integration_test/test_globals.dart
@@ -1,10 +1,13 @@
 
 import 'package:integration_test/integration_test.dart';
 import 'package:saber/data/file_manager/file_manager.dart';
+import 'package:saber/data/flavor_config.dart';
 import 'package:saber/data/prefs.dart';
 
 void initialiseForTests() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  FlavorConfig.setup();
 
   Prefs.testingMode = true;
   Prefs.init();
