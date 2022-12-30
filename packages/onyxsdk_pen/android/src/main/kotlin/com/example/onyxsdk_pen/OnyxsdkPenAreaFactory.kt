9ad3b309d897a1a6949ed46a60d130b1eb71a7c4
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-12-30 22:49:03 +0000

    onyx: create OnyxSdkPenArea widget

---
+++ packages/onyxsdk_pen/android/src/main/kotlin/com/example/onyxsdk_pen/OnyxsdkPenAreaFactory.kt
@@ -0,0 +1,14 @@
+package com.example.onyxsdk_pen
+
+import android.content.Context
+import android.view.View
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+
+class OnyxsdkPenAreaFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        val creationParams = args as Map<String?, Any?>?
+        return OnyxsdkPenArea(context, viewId, creationParams)
+    }
+}
