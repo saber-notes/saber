a44e92bb466ca35b2b0f2096aa5e7c0192abd55c
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-03 20:26:24 +0000

    web: allow exiting fullscreen on Firefox
    See https://github.com/dart-lang/sdk/issues/50857

--- lib/data/web/fullscreen_web.dart
+++ lib/data/web/fullscreen_web.dart
@@ -1,10 +1,17 @@
 
+// ignore: avoid_web_libraries_in_flutter
 import 'dart:html';
+
+import 'fullscreen_web_workarounds.dart';
 
 Future enterFullScreen() async {
   await document.documentElement?.requestFullscreen();
 }
 
 void exitFullscreen() {
-  return document.exitFullscreen();
+  try {
+    (document as JSDocument).exitFullscreen();
+  } catch (e) {
+    document.exitFullscreen();
+  }
 }
