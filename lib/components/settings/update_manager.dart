561fc3b183cd67fc1a5f5de7a0574371269f4feb
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-04 02:40:20 +0000

    version: add extra 0 (#191)
    This lets me replace 5020 with 5021 for Google Play users due to the last-minute bug fix
    
    This won't go into effect until v0.5.3

--- lib/components/settings/update_manager.dart
+++ lib/components/settings/update_manager.dart
@@ -67,7 +67,7 @@ abstract class UpdateManager {
 
     if (newestVersion <= currentVersion) {
       return UpdateStatus.upToDate;
-    } else if (newestVersion == currentVersion + 1 && !kDebugMode) {
+    } else if (newestVersion <= currentVersion + 10 && !kDebugMode) {
       // ignore 1 minor update so the user isn't prompted too often (debug mode ignores this)
       return UpdateStatus.updateOptional;
     } else {
--- lib/data/version.dart
+++ lib/data/version.dart
@@ -1,2 +1,2 @@
 /// The current app version as an ordinal number.
-const int buildNumber = 502;
+const int buildNumber = 5020;
