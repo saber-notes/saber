bda590b9da64c07bd989518bd80372c1e6a1194e
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-04 15:10:24 +0000

    onyx: create empty TouchHelper

--- packages/onyxsdk_pen/android/src/main/kotlin/com/example/onyxsdk_pen/OnyxsdkPenArea.kt
+++ packages/onyxsdk_pen/android/src/main/kotlin/com/example/onyxsdk_pen/OnyxsdkPenArea.kt
@@ -2,23 +2,62 @@ package com.example.onyxsdk_pen
 
 import android.content.Context
 import android.graphics.Color
+import android.util.Log
 import android.view.View
-import android.widget.TextView
+import com.onyx.android.sdk.data.note.TouchPoint
+import com.onyx.android.sdk.pen.RawInputCallback
+import com.onyx.android.sdk.pen.TouchHelper
+import com.onyx.android.sdk.pen.data.TouchPointList
 import io.flutter.plugin.platform.PlatformView
 
 internal class OnyxsdkPenArea(context: Context, id: Int, creationParams: Map<String?, Any?>?) : PlatformView {
-    private val textView: TextView
+    private val touchHelper: TouchHelper
 
+    private val view: View
     override fun getView(): View {
-        return textView
+        return view
     }
 
-    override fun dispose() {}
+    private val callback: RawInputCallback = object: RawInputCallback() {
+        override fun onBeginRawDrawing(b: Boolean, touchPoint: TouchPoint) {
+            // begin of stylus data
+            Log.d("OnyxsdkPenArea", "onBeginRawDrawing")
+        }
+
+        override fun onEndRawDrawing(b: Boolean, touchPoint: TouchPoint) {
+            // end of stylus data
+        }
+
+        override fun onRawDrawingTouchPointMoveReceived(touchPoint: TouchPoint) {
+            // stylus data during stylus moving
+            Log.d("OnyxsdkPenArea", "onRawDrawingTouchPointMoveReceived: " + touchPoint.toString())
+        }
+
+        override fun onRawDrawingTouchPointListReceived(touchPointList: TouchPointList) {
+        }
+
+        override fun onBeginRawErasing(b: Boolean, touchPoint: TouchPoint) {
+        }
+
+        override fun onEndRawErasing(b: Boolean, touchPoint: TouchPoint) {
+        }
+
+        override fun onRawErasingTouchPointMoveReceived(touchPoint: TouchPoint) {
+        }
+
+        override fun onRawErasingTouchPointListReceived(touchPointList: TouchPointList) {
+        }
+    }
 
     init {
-        textView = TextView(context)
-        textView.textSize = 36f
-        textView.setBackgroundColor(Color.argb(150, 255, 255, 255))
-        textView.text = "Rendered on a native Android view (id: $id)"
+        view = View(context)
+        touchHelper = TouchHelper.create(view, callback)
+        touchHelper.setStrokeWidth(3.0f)
+        touchHelper.openRawDrawing()
+        touchHelper.setRawDrawingEnabled(true)
+    }
+
+    override fun dispose() {
+        touchHelper.closeRawDrawing()
     }
 }
