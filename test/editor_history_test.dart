c3ffc71ccf61e9d820ef5f28898b595724e1e877
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-07 01:08:24 +0000

    test: editor history

---
+++ test/editor_history_test.dart
@@ -0,0 +1,50 @@
+
+import 'package:saber/data/editor/editor_history.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("Test editor history", () {
+    final EditorHistory history = EditorHistory();
+
+    final EditorHistoryItem item1 = EditorHistoryItem(
+      type: EditorHistoryItemType.draw,
+      strokes: [],
+      images: [],
+    );
+    final EditorHistoryItem item2 = EditorHistoryItem(
+      type: EditorHistoryItemType.draw,
+      strokes: [],
+      images: [],
+    );
+
+    expect(history.canUndo, false, reason: "History should be empty");
+    expect(history.canRedo, false, reason: "History should be empty");
+
+    expect(() => history.undo(), throwsA(anything), reason: "Undo should throw an exception if history is empty");
+    expect(() => history.redo(), throwsA(anything), reason: "Redo should throw an exception if history is empty");
+
+    history.recordChange(item1);
+    expect(history.canUndo, true);
+    expect(history.canRedo, false);
+
+    expect(history.undo(), item1, reason: "Undo should return the last item");
+    history.canRedo = true;
+    expect(history.canUndo, false);
+    expect(history.canRedo, true);
+
+    expect(history.redo(), item1, reason: "Redo should return the last item");
+    expect(history.canUndo, true);
+    expect(history.canRedo, false);
+
+    expect(history.undo(), item1, reason: "Undo should return the last item");
+    history.canRedo = true;
+    expect(history.canUndo, false);
+    expect(history.canRedo, true);
+
+    history.recordChange(item2);
+    expect(history.canUndo, true);
+    expect(history.canRedo, false);
+
+    expect(history.undo(), item2, reason: "Undo should return the last item");
+  });
+}
