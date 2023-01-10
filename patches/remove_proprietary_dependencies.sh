b05b42b35939cd6b88675b7b8c32a888dd7a0fb5
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-10 16:24:30 +0000

    fdroid: remove packages/onyxsdk_pen

--- patches/remove_proprietary_dependencies.sh
+++ patches/remove_proprietary_dependencies.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # find "path: packages/onyxsdk_pen" and replace with "path: packages/onyxsdk_pen_dummy"
+echo "Replacing onyxsdk_pen with onyxsdk_pen_dummy"
 sed -i -e 's/path: packages\/onyxsdk_pen/path: packages\/onyxsdk_pen_dummy/g' pubspec.yaml
+
+# Delete packages/onyxsdk_pen to make sure it's not used
+echo "Deleting packages/onyxsdk_pen"
+rm -rf packages/onyxsdk_pen
