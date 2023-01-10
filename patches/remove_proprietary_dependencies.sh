c630a983740bccb02c09babccef794eda2a68340
Licaon_Kter <licaon.kter@protonmail.com>
2023-01-10 16:31:58 +0000

    Update remove_proprietary_dependencies.sh

--- patches/remove_proprietary_dependencies.sh
+++ patches/remove_proprietary_dependencies.sh
@@ -2,7 +2,11 @@
 
 # find "path: packages/onyxsdk_pen" and replace with "path: packages/onyxsdk_pen_dummy"
 echo "Replacing onyxsdk_pen with onyxsdk_pen_dummy"
-sed -i -e 's/path: packages\/onyxsdk_pen/path: packages\/onyxsdk_pen_dummy/g' pubspec.yaml
+sed -i -e 's!/onyxsdk_pen!/onyxsdk_pen_dummy!' pubspec.yaml
+
+# remove Onyx maven repo
+echo "Removing Onyx,jitpack maven repo"
+sed -i -e '18,24d' android/build.gradle
 
 # Delete packages/onyxsdk_pen to make sure it's not used
 echo "Deleting packages/onyxsdk_pen"
