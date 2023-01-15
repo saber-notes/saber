0e5be3fd27da246ff4b1b3e5c21371ee4fcf168a
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-15 02:59:47 +0000

    onyx: only patch onyxsdk_pen_dummy once

--- patches/remove_proprietary_dependencies.sh
+++ patches/remove_proprietary_dependencies.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # find "path: packages/onyxsdk_pen" and replace with "path: packages/onyxsdk_pen_dummy"
-echo "Replacing onyxsdk_pen with onyxsdk_pen_dummy"
-sed -i -e 's!/onyxsdk_pen!/onyxsdk_pen_dummy!' pubspec.yaml
+echo -n "Replacing onyxsdk_pen with onyxsdk_pen_dummy: "
+# check if pubspec.yaml contains onyxsdk_pen_dummy
+if grep -q "onyxsdk_pen_dummy" pubspec.yaml; then
+  echo "already done"
+else
+  echo "found"
+  sed -i -e 's!/onyxsdk_pen!/onyxsdk_pen_dummy!' pubspec.yaml
+fi
 
 # remove Onyx maven repo
 echo -n "Removing Onyx,jitpack maven repo: "
@@ -11,7 +17,7 @@ if grep -q "repo.boox.com" "android/build.gradle"; then
   echo "found"
   sed -i -e '18,24d' android/build.gradle
 else
-  echo "already removed"
+  echo "already done"
 fi
 
 # Delete packages/onyxsdk_pen to make sure it's not used
