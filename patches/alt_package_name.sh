599f2e1850ff397613b07fef0bf0623746312dc4
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-18 04:41:12 +0000

    android: build with alt package name

---
+++ patches/alt_package_name.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Change applicationId to "com.adilhanney.saber.alt"
+echo "Changing applicationId to com.adilhanney.saber.alt"
+sed -i "s/applicationId \"com.adilhanney.saber\"/applicationId \"com.adilhanney.saber.alt\"/g" android/app/build.gradle
