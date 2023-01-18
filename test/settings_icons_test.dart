d7304ae7e84a514f3d2892a9d039cdbb9f443593
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-18 02:55:59 +0000

    test: adapt settings icon test for FaIcons

--- test/settings_icons_test.dart
+++ test/settings_icons_test.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:saber/data/flavor_config.dart';
 import 'package:saber/data/prefs.dart';
 import 'package:saber/pages/home/settings.dart';
@@ -16,11 +17,11 @@ void main() {
     await tester.pumpAndSettle();
 
     // Static icons
-    expect(find.byIcon(Icons.language), findsAtLeastNWidgets(1));
-    expect(find.byIcon(Icons.colorize), findsAtLeastNWidgets(1));
-    expect(find.byIcon(Icons.system_update), findsAtLeastNWidgets(1));
-    expect(find.byIcon(Icons.fullscreen), findsAtLeastNWidgets(1));
-    expect(find.byIcon(Icons.cleaning_services), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.language), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.colorize), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.system_update), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.fullscreen), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.cleaning_services), findsAtLeastNWidgets(1));
 
     // Dynamic icons...
 
@@ -28,63 +29,75 @@ void main() {
     // findsAtLeastNWidgets(2) because of leading + trailing icons
     Prefs.appTheme.value = ThemeMode.system.index;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.brightness_auto), findsAtLeastNWidgets(2));
+    expect(findIcon(Icons.brightness_auto), findsAtLeastNWidgets(2));
     Prefs.appTheme.value = ThemeMode.light.index;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.light_mode), findsAtLeastNWidgets(2));
+    expect(findIcon(Icons.light_mode), findsAtLeastNWidgets(2));
     Prefs.appTheme.value = ThemeMode.dark.index;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.dark_mode), findsAtLeastNWidgets(2));
+    expect(findIcon(Icons.dark_mode), findsAtLeastNWidgets(2));
 
     // Hyperlegible font
     Prefs.hyperlegibleFont.value = false;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.font_download_off), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.font_download_off), findsAtLeastNWidgets(1));
     Prefs.hyperlegibleFont.value = true;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.font_download), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.font_download), findsAtLeastNWidgets(1));
 
     // Editor toolbar on bottom
     Prefs.editorToolbarOnBottom.value = false;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.vertical_align_top), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.vertical_align_top), findsAtLeastNWidgets(1));
     Prefs.editorToolbarOnBottom.value = true;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.vertical_align_bottom), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.vertical_align_bottom), findsAtLeastNWidgets(1));
 
     // Editor auto invert
     Prefs.editorAutoInvert.value = false;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.invert_colors_off), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.invert_colors_off), findsAtLeastNWidgets(1));
     Prefs.editorAutoInvert.value = true;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.invert_colors_on), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.invert_colors_on), findsAtLeastNWidgets(1));
 
     // Prefer greyscale
     Prefs.preferGreyscale.value = false;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.enhance_photo_translate), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.enhance_photo_translate), findsAtLeastNWidgets(1));
     Prefs.preferGreyscale.value = true;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.monochrome_photos), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.monochrome_photos), findsAtLeastNWidgets(1));
 
     // Editor straighten lines
     Prefs.editorStraightenDelay.value = 0;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.gesture), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.gesture), findsAtLeastNWidgets(1));
     Prefs.editorStraightenDelay.value = 500;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.straighten), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.straighten), findsAtLeastNWidgets(1));
     Prefs.editorStraightenDelay.value = 1000;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.straighten), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.straighten), findsAtLeastNWidgets(1));
 
     // Editor prompt rename
     Prefs.editorPromptRename.value = false;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.keyboard_hide), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.keyboard_hide), findsAtLeastNWidgets(1));
     Prefs.editorPromptRename.value = true;
     await tester.pumpAndSettle();
-    expect(find.byIcon(Icons.keyboard), findsAtLeastNWidgets(1));
+    expect(findIcon(Icons.keyboard), findsAtLeastNWidgets(1));
+  });
+}
+
+/// Finds an [Icon] or [FaIcon] widget with the given IconData.
+Finder findIcon(IconData icon) {
+  return find.byWidgetPredicate((widget) {
+    if (widget is Icon) {
+      return widget.icon == icon;
+    } else if (widget is FaIcon) {
+      return widget.icon == icon;
+    }
+    return false;
   });
 }
