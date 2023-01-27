cbe6709b0991f57de83e34cdf3afcafd39cb6af9
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-01-27 01:53:18 +0000

    test: locale names should exist for all supported locales

---
+++ test/locale_names_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:saber/data/locales.dart';
+import 'package:saber/i18n/strings.g.dart';
+
+void main() {
+  test("Test that all supported languages have a localised name", () {
+    for (Locale locale in AppLocaleUtils.supportedLocales) {
+      final String localeCode = locale.toLanguageTag();
+      assert(localeNames.containsKey(localeCode), "Missing locale name for $localeCode");
+    }
+  });
+}
