ff398d5dcd6dd06160567e1676a2a56ff609781f
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-06 21:54:39 +0000

    whiteboard: fix going into fullscreen

--- lib/pages/home/whiteboard.dart
+++ lib/pages/home/whiteboard.dart
@@ -1,21 +1,21 @@
-
 import 'package:flutter/material.dart';
 import 'package:saber/data/prefs.dart';
 import 'package:saber/i18n/strings.g.dart';
 import 'package:saber/pages/editor/editor.dart';
 
 class Whiteboard extends StatelessWidget {
-  const Whiteboard({
-    super.key = const ValueKey("whiteboard"),
-  });
+  const Whiteboard({super.key});
 
   static const String filePath = "/_whiteboard";
 
   static bool needsToAutoClearWhiteboard = Prefs.autoClearWhiteboardOnExit.value;
 
+  static final _whiteboardKey = GlobalKey(debugLabel: "whiteboard");
+
   @override
   Widget build(BuildContext context) {
     return Editor(
+      key: _whiteboardKey,
       path: filePath,
       customTitle: t.home.titles.whiteboard,
     );
