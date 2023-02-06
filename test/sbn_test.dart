185ec658bfff9dabca6f6250538cfdb83c337b81
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-06 17:11:17 +0000

    test: sbn parsing of quill

--- test/sbn_test.dart
+++ test/sbn_test.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:saber/components/canvas/_canvas_background_painter.dart';
 import 'package:saber/components/canvas/tools/pen.dart';
@@ -8,11 +10,11 @@ import 'package:saber/data/editor/editor_core_info.dart';
 import 'utils/test_mock_channel_handlers.dart';
 
 void main() {
-  group('Test SBN parsing', () {
-    test('v9 single stroke', () async {
-      TestWidgetsFlutterBinding.ensureInitialized();
-      setupMockPathProvider();
+  group('Test SBN parsing:', () {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    setupMockPathProvider();
 
+    test('v9 single stroke', () async {
       const path = 'test/sbn_examples/v9_single_stroke.sbn';
       File file = File(path);
       String contents = await file.readAsString();
@@ -64,6 +66,65 @@ void main() {
       expect(stroke.points[2].p, 0.9);
       expect(stroke.points[2].x, 555.555555555555);
       expect(stroke.points[2].y, 555.555555555555);
+    });
+
+    test('v9 quill', () async {
+      const path = 'test/sbn_examples/v9_quill.sbn';
+      File file = File(path);
+      String contents = await file.readAsString();
+
+      EditorCoreInfo coreInfo = await EditorCoreInfo.loadFromFileContents(
+        contents,
+        path: path,
+        readOnly: true,
+        onlyFirstPage: false,
+      );
+
+      // make sure the file was loaded
+      expect(coreInfo.pages.length, greaterThan(0), reason: 'Failed to load v9_quill.sbn');
+
+      expect(coreInfo.backgroundColor, null);
+      expect(coreInfo.initialPageIndex, 0);
+      expect(coreInfo.lineHeight, 90);
+      expect(coreInfo.nextImageId, 0);
+      expect(coreInfo.backgroundPattern, CanvasBackgroundPatterns.lined);
+
+      expect(coreInfo.pages.length, 2);
+      expect(coreInfo.pages[0].isEmpty, false);
+      expect(coreInfo.pages[1].isEmpty, true);
+
+      final page = coreInfo.pages[0];
+      expect(page.size.width, 1000);
+      expect(page.size.height, 1400);
+      expect(page.strokes.length, 0);
+      expect(page.images.length, 0);
+      expect(page.quill.controller.document.isEmpty(), false);
+
+      final controller = page.quill.controller;
+      controller.updateSelection(const TextSelection(
+        baseOffset: 0,
+        extentOffset: 100000000,
+      ), ChangeSource.LOCAL);
+      final plainText = controller.getPlainText();
+      expect(plainText, 'h1\nh2\nh3\nn');
+
+      bool foundH1 = false,
+          foundH2 = false,
+          foundH3 = false;
+      for (final Operation operation in controller.document.toDelta().toList()) {
+        final header = operation.attributes?["header"];
+        if (header == null) continue;
+        if (header == 1) {
+          foundH1 = true;
+        } else if (header == 2) {
+          foundH2 = true;
+        } else if (header == 3) {
+          foundH3 = true;
+        }
+      }
+      expect(foundH1, true);
+      expect(foundH2, true);
+      expect(foundH3, true);
     });
   });
 }
