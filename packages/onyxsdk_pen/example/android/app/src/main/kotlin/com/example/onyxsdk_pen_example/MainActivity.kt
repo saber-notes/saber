839f4e4965a59c03cc1148c822e0cf3376f45976
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-07 14:20:34 +0000

    onyx: use onyxsdk_pen_dummy

--- packages/onyxsdk_pen/example/android/app/src/main/kotlin/com/example/onyxsdk_pen_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.onyxsdk_pen_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- packages/onyxsdk_pen/lib/onyxsdk_pen.dart
+++
@@ -1,7 +0,0 @@
-import 'onyxsdk_pen_platform_interface.dart';
-
-class OnyxsdkPen {
-  Future<bool> isOnyxDevice() {
-    return OnyxsdkPenPlatform.instance.isOnyxDevice();
-  }
-}
