47a89827c5d2a68b6e55646b93473f24541e01ad
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-10 17:22:04 +0000

    i18n: apply strings

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -1,9 +1,9 @@
 /// Generated file. Do not edit.
 ///
 /// Locales: 10
-/// Strings: 1304 (130 per locale)
+/// Strings: 1307 (130 per locale)
 ///
-/// Built on 2023-02-10 at 17:15 UTC
+/// Built on 2023-02-10 at 17:21 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
