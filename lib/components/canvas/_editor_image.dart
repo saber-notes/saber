d7f4505b9e5dfe30cca76d51221f6f4516949d50
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-12 16:18:46 +0000

    pages: allow reordering pages by drag and drop

--- lib/components/canvas/_editor_image.dart
+++ lib/components/canvas/_editor_image.dart
@@ -43,7 +43,7 @@ class EditorImage {
     }
   }
 
-  final int pageIndex;
+  int pageIndex;
   void Function(EditorImage, Rect)? onMoveImage;
   void Function(EditorImage)? onDeleteImage;
   void Function()? onMiscChange;
