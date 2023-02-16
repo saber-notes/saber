cc8c7f200e3c4b85be24d612fb11696186004918
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-16 03:24:42 +0000

    EditorImage: create subclass for svg images

--- lib/components/canvas/color_extensions.dart
+++ lib/components/canvas/color_extensions.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 extension ColorExtensions on Color {
   /// Inverts the color's HSL lightness if [invert] is true.
   /// This does not affect the hue or saturation.
-  withInversion([bool invert = true]) {
+  Color withInversion([bool invert = true]) {
     if (!invert) return this;
 
     final HSLColor hsl = HSLColor.fromColor(this);
@@ -12,7 +12,7 @@ extension ColorExtensions on Color {
   }
 
   /// Multiplies the color's HSL saturation by [saturationMultiplier].
-  withSaturation(double saturationMultiplier) {
+  Color withSaturation(double saturationMultiplier) {
     final HSLColor hsl = HSLColor.fromColor(this);
     return hsl.withSaturation(hsl.saturation * saturationMultiplier).toColor();
   }
