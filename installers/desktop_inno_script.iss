4df07480875c1e11881d622a94598d454e978a32
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-02-25 00:05:41 +0000

    v0.9.5

--- installers/desktop_inno_script.iss
+++ installers/desktop_inno_script.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Saber"
-#define MyAppVersion "0.9.4"
+#define MyAppVersion "0.9.5"
 #define MyAppPublisher "Adil Hanney"
 #define MyAppURL "https://github.com/adil192/saber"
 #define MyAppExeName "saber.exe"
--- lib/data/version.dart
+++ lib/data/version.dart
@@ -2,10 +2,10 @@
 // Run `./apply_version.sh --help` for more information.
 
 /// The current app version as an ordinal number.
-const int buildNumber = 9040;
+const int buildNumber = 9050;
 
 /// The current app version as a string.
-const String buildName = '0.9.4';
+const String buildName = '0.9.5';
 
 /// The year in which the current version was released.
 const int buildYear = 2023;
