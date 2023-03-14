f0b68eae166af7771875d11f63900233475d2d55
Adil Hanney <adilhanney@disroot.org>
2023-03-14 05:54:04 +0000

    i18n: regenerate after slang upgrade

--- lib/i18n/strings.g.dart
+++ lib/i18n/strings.g.dart
@@ -3,7 +3,7 @@
 /// Locales: 13
 /// Strings: 1885 (145 per locale)
 ///
-/// Built on 2023-03-10 at 19:44 UTC
+/// Built on 2023-03-14 at 05:52 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
@@ -79,10 +79,7 @@ class Translations {
 
 /// The provider for method B
 class TranslationProvider extends BaseTranslationProvider<AppLocale, _StringsEn> {
-	TranslationProvider({required super.child}) : super(
-		initLocale: LocaleSettings.instance.currentLocale,
-		initTranslations: LocaleSettings.instance.currentTranslations,
-	);
+	TranslationProvider({required super.child}) : super(settings: LocaleSettings.instance);
 
 	static InheritedLocaleData<AppLocale, _StringsEn> of(BuildContext context) => InheritedLocaleData.of<AppLocale, _StringsEn>(context);
 }
@@ -105,8 +102,8 @@ class LocaleSettings extends BaseFlutterLocaleSettings<AppLocale, _StringsEn> {
 	// static aliases (checkout base methods for documentation)
 	static AppLocale get currentLocale => instance.currentLocale;
 	static Stream<AppLocale> getLocaleStream() => instance.getLocaleStream();
-	static AppLocale setLocale(AppLocale locale) => instance.setLocale(locale);
-	static AppLocale setLocaleRaw(String rawLocale) => instance.setLocaleRaw(rawLocale);
+	static AppLocale setLocale(AppLocale locale, {bool? listenToDeviceLocale = false}) => instance.setLocale(locale, listenToDeviceLocale: listenToDeviceLocale);
+	static AppLocale setLocaleRaw(String rawLocale, {bool? listenToDeviceLocale = false}) => instance.setLocaleRaw(rawLocale, listenToDeviceLocale: listenToDeviceLocale);
 	static AppLocale useDeviceLocale() => instance.useDeviceLocale();
 	@Deprecated('Use [AppLocaleUtils.supportedLocales]') static List<Locale> get supportedLocales => instance.supportedLocales;
 	@Deprecated('Use [AppLocaleUtils.supportedLocalesRaw]') static List<String> get supportedLocalesRaw => instance.supportedLocalesRaw;
