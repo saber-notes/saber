3dea72e6eb0918093d2ce1c76bf93f7811590443
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-04-03 05:29:27 +0100

    v0.11.0

--- lib/data/version.dart
+++ lib/data/version.dart
@@ -2,10 +2,10 @@
 // Run `./apply_version.sh --help` for more information.
 
 /// The current app version as an ordinal number.
-const int buildNumber = 10000;
+const int buildNumber = 11000;
 
 /// The current app version as a string.
-const String buildName = '0.10.0';
+const String buildName = '0.11.0';
 
 /// The year in which the current version was released.
 const int buildYear = 2023;
