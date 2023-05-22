b7a50bcf38f9f2a45e848be30ac6cb777221fef9
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-05-22 13:51:43 +0100

    test: also check for misspellings in _missing_translations

--- test/i18n_misspellings_test.dart
+++ test/i18n_misspellings_test.dart
@@ -4,8 +4,10 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Check that Saber isn\'t misspelled', () {
-    final translationsDir = Directory('lib/i18n/community');
-    final List<FileSystemEntity> files = translationsDir.listSync().toList();
+    final List<FileSystemEntity> files = [
+      File('lib/i18n/_missing_translations.json'),
+      ...Directory('lib/i18n/community').listSync(),
+    ];
 
     for (final file in files) {
       test('in ${file.path}', () async {
