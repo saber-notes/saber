3bd5aee9c806e709a0f3dd9309e97a09b214a5ae
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-06-03 05:11:07 +0100

    fix(editor): make "back" button go home on whiteboard

--- lib/components/canvas/save_indicator.dart
+++ lib/components/canvas/save_indicator.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:saber/data/routes.dart';
 
 /// Replaces the back button as the
 /// [AppBar.leading] widget in the [AppBar]
@@ -25,10 +27,21 @@ class SaveIndicator extends StatelessWidget {
         SavingState.saving => const CircularProgressIndicator(),
         SavingState.saved => IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.of(context).pop(),
+          onPressed: () => _back(context),
         ),
       },
     );
+  }
+
+  void _back(BuildContext context) {
+    final navigator = Navigator.of(context);
+    final isWhiteboard = !navigator.canPop();
+    if (isWhiteboard) {
+      // if on whiteboard, go to "recents" tab of home screen
+      context.go(HomeRoutes.getRoute(0));
+    } else {
+      navigator.pop();
+    }
   }
 }
 
