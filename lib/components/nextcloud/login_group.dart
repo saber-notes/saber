b6e61673a0dae2d3c592b2840a50f29fbb3f34f3
Adil Hanney <adilhanney@disroot.org>
2023-06-12 01:56:30 +0100

    chore: run `dart fix`

--- lib/components/nextcloud/login_group.dart
+++ lib/components/nextcloud/login_group.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:collapsible/collapsible.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
--- lib/pages/user/profile_page.dart
+++ lib/pages/user/profile_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:saber/components/misc/faq.dart';
 import 'package:saber/data/prefs.dart';
