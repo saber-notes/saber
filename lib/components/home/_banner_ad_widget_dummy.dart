1a42116c4c780520dd4605ad78a79fe695734ba7
Adil Hanney <adilhanney@disroot.org>
2023-07-08 20:13:30 +0100

    chore: add deprecation messages to dummy ad classes

--- lib/components/home/_banner_ad_widget_dummy.dart
+++ lib/components/home/_banner_ad_widget_dummy.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
+const _kDummyAdMessage = 'This is a dummy class for when ads are disabled.';
+
+@Deprecated(_kDummyAdMessage)
 abstract class AdState {
+  @Deprecated(_kDummyAdMessage)
   static bool get adsSupported => false;
 
+  @Deprecated(_kDummyAdMessage)
   static void init() {}
 
+  @Deprecated(_kDummyAdMessage)
   static void showConsentForm() {}
 }
 
+@Deprecated(_kDummyAdMessage)
 class BannerAdWidget extends StatelessWidget {
   const BannerAdWidget({
     super.key,
@@ -22,6 +29,7 @@ class BannerAdWidget extends StatelessWidget {
   }
 }
 
+@Deprecated(_kDummyAdMessage)
 class AdSize {
   const AdSize({
     required this.width,
