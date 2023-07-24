b083f7c9f754d804027d51b350178b376280524a
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-07-24 20:25:27 +0100

    fix: mispelling of Saber in ru full_description

--- test/i18n_misspellings_test.dart
+++ test/i18n_misspellings_test.dart
@@ -7,6 +7,13 @@ void main() {
     final List<FileSystemEntity> files = [
       File('lib/i18n/_missing_translations.yaml'),
       ...Directory('lib/i18n/community').listSync(),
+
+      // All files of the format metadata/XX/full_description.txt
+      ...Directory('metadata').listSync()
+          .whereType<Directory>()
+          .expand((dir) => dir.listSync())
+          .whereType<File>()
+          .where((file) => file.path.endsWith('full_description.txt')),
     ];
 
     for (final file in files) {
