9dd5d743e3f608f96fbd2b41dfc2fd8b43013983
Adil Hanney <adilhanney@disroot.org>
2023-07-28 08:17:37 +0100

    v0.14.11+1
    iOS update required ATT

--- lib/data/version.dart
+++ lib/data/version.dart
@@ -2,7 +2,7 @@
 // Run `./scripts/apply_version.sh --help` for more information.
 
 /// The current app version as an ordinal number.
-const int buildNumber = 14110;
+const int buildNumber = 14111;
 
 /// The current app version as a string.
 const String buildName = '0.14.11';
