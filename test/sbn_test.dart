fa346d3f7e72a2e47f49bd29a3dc0e0501fbb291
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-11-03 01:13:04 +0000

    test: also test inverted sbns

--- test/sbn_test.dart
+++ test/sbn_test.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:saber/components/canvas/canvas.dart';
+import 'package:saber/components/canvas/invert_shader.dart';
 import 'package:saber/data/editor/editor_core_info.dart';
+import 'package:saber/data/editor/page.dart';
 import 'package:saber/data/file_manager/file_manager.dart';
 import 'package:saber/data/flavor_config.dart';
 import 'package:saber/data/prefs.dart';
@@ -22,6 +24,10 @@ void main() {
     Prefs.testingMode = true;
     Prefs.init();
     FileManager.init();
+
+    setUpAll(() async {
+      await InvertShader.init();
+    });
 
     final sbnExamples = Directory('test/sbn_examples/')
         .listSync()
@@ -59,34 +65,12 @@ void main() {
         addTearDown(tester.view.resetPhysicalSize);
 
         // create Canvas widget
-        await tester.pumpWidget(
-          TranslationProvider(
-            child: SizedBox(
-              width: page.size.width,
-              height: page.size.height,
-              child: MaterialApp(
-                home: SizedBox(
-                  width: page.size.width,
-                  height: page.size.height,
-                  child: RepaintBoundary(
-                    child: Canvas(
-                      path: path,
-                      page: page,
-                      pageIndex: 0,
-                      textEditing: false,
-                      coreInfo: coreInfo,
-                      currentStroke: null,
-                      currentStrokeDetectedShape: null,
-                      currentSelection: null,
-                      setAsBackground: null,
-                      currentToolIsSelect: false,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        );
+        await tester.pumpWidget(_buildCanvas(
+          brightness: Brightness.light,
+          path: path,
+          page: page,
+          coreInfo: coreInfo,
+        ));
 
         // precache images
         final context = tester.binding.rootElement!;
@@ -103,7 +87,57 @@ void main() {
           find.byType(Canvas),
           matchesGoldenFile('sbn_examples/$sbnName.png'),
         );
+
+        // now test dark mode
+        await tester.pumpWidget(_buildCanvas(
+          brightness: Brightness.dark,
+          path: path,
+          page: page,
+          coreInfo: coreInfo,
+        ));
+        await tester.pumpAndSettle();
+        await expectLater(
+          find.byType(Canvas),
+          matchesGoldenFile('sbn_examples/$sbnName.dark.png'),
+        );
       });
     }
   });
+}
+
+Widget _buildCanvas({
+  required Brightness brightness,
+  required String path,
+  required EditorPage page,
+  required EditorCoreInfo coreInfo,
+}) {
+  return TranslationProvider(
+    child: SizedBox(
+      width: page.size.width,
+      height: page.size.height,
+      child: MaterialApp(
+        theme: ThemeData(
+          brightness: brightness,
+        ),
+        home: SizedBox(
+          width: page.size.width,
+          height: page.size.height,
+          child: RepaintBoundary(
+            child: Canvas(
+              path: coreInfo.filePath,
+              page: page,
+              pageIndex: 0,
+              textEditing: false,
+              coreInfo: coreInfo,
+              currentStroke: null,
+              currentStrokeDetectedShape: null,
+              currentSelection: null,
+              setAsBackground: null,
+              currentToolIsSelect: false,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
