30cd7f73bcf011e5f1ee8324b3be22847eb39ed8
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-12-04 14:48:22 +0000

    chore: enforce offline fonts while debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:saber/data/flavor_config.dart';
 import 'package:saber/main_common.dart' as common;
@@ -16,12 +17,13 @@ Future<void> main() async {
     shouldCheckForUpdatesByDefault: const bool.fromEnvironment('UPDATE_CHECK', defaultValue: true),
     dirty: const bool.fromEnvironment('DIRTY', defaultValue: false),
   );
-  
-  if (const bool.fromEnvironment('OFFLINE_FONTS_ONLY', defaultValue: false)) {
+
+  if (const bool.fromEnvironment('OFFLINE_FONTS_ONLY', defaultValue: kDebugMode)) {
     // All fonts should already be included (offline) in the app, but in case
     // I've forgot to add one, it'll be fetched from the internet.
     // This prevents the app from fetching fonts from Google Servers,
     // to comply with F-Droid requirements.
+    // Offline fonts are also enforced by default in debug mode.
     GoogleFonts.config.allowRuntimeFetching = false;
   }
 
