41495587c1018651c94a05ddb34c8cfa84eefd3f
Adil Hanney <adilh+github@orchid.anonaddy.com>
2023-12-11 20:39:07 +0000

    test: speed up mispellings test

--- test/i18n_misspellings_test.dart
+++ test/i18n_misspellings_test.dart
@@ -3,10 +3,10 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Check that Saber isn\'t misspelled', () {
-    final List<FileSystemEntity> files = [
+  test('Check that Saber isn\'t misspelled', () async {
+    final List<File> files = [
       File('lib/i18n/_missing_translations.yaml'),
-      ...Directory('lib/i18n/community').listSync(),
+      ...Directory('lib/i18n/community').listSync().whereType<File>(),
 
       // All files of the format metadata/XX/full_description.txt
       ...Directory('metadata')
@@ -17,12 +17,12 @@ void main() {
           .where((file) => file.path.endsWith('full_description.txt')),
     ];
 
-    for (final file in files) {
-      test('in ${file.path}', () async {
-        final contents = await File(file.path).readAsString();
-        expect(contents, isNot(contains('Sabre')),
-            reason: 'Saber is misspelled as Sabre in ${file.path}');
-      });
-    }
+    await Future.wait([
+      for (final file in files)
+        file.readAsString().then((contents) {
+          expect(contents, isNot(contains('Sabre')),
+              reason: 'Saber is misspelled as Sabre in ${file.path}');
+        }),
+    ]);
   });
 }
