4a944490db38a67723d70703672fbae2a8839664
Adil Hanney <adilh+github@orchid.anonaddy.com>
2024-02-14 19:29:48 +0000

    chore: lint fixes

--- lib/components/toolbar/toolbar.dart
+++ lib/components/toolbar/toolbar.dart
@@ -1,6 +1,5 @@
 import 'package:collapsible/collapsible.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_quill/flutter_quill.dart';
