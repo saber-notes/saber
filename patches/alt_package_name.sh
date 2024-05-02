c48612e2ddaac4d05c1499c48247d03f7cc643f3
Adil Hanney <adilh+github@orchid.anonaddy.com>
2024-05-02 06:19:10 +0100

    github: remove build-foss-alt-apk

--- patches/alt_package_name.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Change applicationId to "com.adilhanney.saber.alt"
-echo "Changing applicationId to com.adilhanney.saber.alt"
-sed -i "s/applicationId \"com.adilhanney.saber\"/applicationId \"com.adilhanney.saber.alt\"/g" android/app/build.gradle
