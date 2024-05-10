fdec33907e7fb400ca55140ab2b7afa39d5e91f6
Adil Hanney <adilhanney@disroot.org>
2024-05-10 14:23:15 +0100

    test: apply_version shouldn't make changes

--- test/version_test.dart
+++ test/version_test.dart
@@ -11,6 +11,25 @@ import 'package:saber/data/version.dart';
 const String dummyChangelog = 'Release_notes_will_be_added_here';
 
 void main() {
+  test('Does apply_version.dart find changes needed?', () async {
+    final result = await Process.run('./scripts/apply_version.dart', [
+      '--custom',
+      buildName,
+      '--fail-on-changes',
+      '--quiet',
+    ]);
+    printOnFailure(result.stdout);
+    printOnFailure(result.stderr);
+
+    final exitCode = result.exitCode;
+    if (exitCode != 0 && exitCode != 10) {
+      throw Exception('Unexpected exit code: $exitCode');
+    }
+    expect(exitCode, isNot(equals(10)),
+        reason: 'Changes needed to be made. '
+            'Please re-run `./scripts/apply_version.dart`');
+  });
+
   test('Check for dummy text in changelogs', () async {
     final File androidMetadata =
         File('metadata/en-US/changelogs/$buildNumber.txt');
