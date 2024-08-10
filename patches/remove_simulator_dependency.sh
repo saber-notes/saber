321df630f792d810b2110fa1e67dd9b44febf4ba
Adil Hanney <adilhanney@disroot.org>
2024-08-10 08:12:51 +0100

    patches: replace main_simulator.dart with main.dart

--- patches/remove_simulator_dependency.sh
+++ patches/remove_simulator_dependency.sh
@@ -3,5 +3,5 @@
 # Remove `simulator` from pubspec.yaml
 sed -i -e '/simulator/d' pubspec.yaml
 
-# Delete `main_simulator.dart` entry point
-rm lib/main_simulator.dart
+# Delete `main_simulator.dart` entry point (replace with `main.dart`)
+cp lib/main.dart lib/main_simulator.dart
