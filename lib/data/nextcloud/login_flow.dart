643103f56a3cdfd97696d7a4509def2fc60aa816
Adil Hanney <adilhanney@disroot.org>
2024-08-23 23:48:53 +0100

    chore: update dependencies

--- lib/data/nextcloud/login_flow.dart
+++ lib/data/nextcloud/login_flow.dart
@@ -62,7 +62,11 @@ class SaberLoginFlow {
         const Duration(seconds: 1),
         (_) => _catch404Error(() async {
           // Throws 404 if not logged in yet
-          final poll = await flowClient.poll(token: init!.poll.token);
+          final poll = await flowClient.poll(
+            $body: ClientFlowLoginV2PollRequestApplicationJson(
+              (b) => b..token = init!.poll.token,
+            ),
+          );
 
           _pollTimer?.cancel();
           if (!completer.isCompleted) completer.complete(poll.body);
