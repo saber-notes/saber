98593a8cd13c2d0d0d4e1eeb40b457e9336cede0
Adil Hanney <adilhanney@disroot.org>
2025-08-03 18:03:22 +0100

    feat(sentry): redact domain names and local files

--- lib/data/sentry_filter.dart
+++ lib/data/sentry_filter.dart
@@ -3,11 +3,15 @@ import 'dart:async';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 abstract class SentryFilter {
+  static final saberSyncFileRegex = RegExp(r'SaberSyncFile\(.+\)');
+  static final remoteFileRegex = RegExp(r'([a-zA-Z0-9]+\.sbe)');
+  static final localFileRegex = RegExp(r'/[a-zA-Z0-9_.-]+\.sbn2');
+  static final domainRegex = RegExp(r'://[a-zA-Z0-9.-]+');
+
   static FutureOr<SentryEvent?> beforeSend(SentryEvent event, Hint hint) async {
     var message = event.message?.formatted;
     if (message != null) {
       // Remove SaberSyncFile(...)
-      final saberSyncFileRegex = RegExp(r'SaberSyncFile\(.+\)');
       if (message.contains(saberSyncFileRegex)) {
         message =
             message.replaceAll(saberSyncFileRegex, 'SaberSyncFile([redacted])');
@@ -15,9 +19,20 @@ abstract class SentryFilter {
       }
 
       // Redact remote file paths
-      final remoteFileRegex = RegExp(r'([a-zA-Z0-9]+\.sbe)');
       if (message.contains(remoteFileRegex)) {
         message = message.replaceAll(remoteFileRegex, '[redacted].sbe');
+        event.message = SentryMessage(message);
+      }
+
+      // Redact local file paths
+      if (message.contains(localFileRegex)) {
+        message = message.replaceAll(localFileRegex, '/[redacted].sbn2');
+        event.message = SentryMessage(message);
+      }
+
+      // Redact domain names
+      if (message.contains(domainRegex)) {
+        message = message.replaceAll(domainRegex, '://[redacted]');
         event.message = SentryMessage(message);
       }
     }
--- test/sentry_filter_test.dart
+++ test/sentry_filter_test.dart
@@ -5,8 +5,7 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 void main() {
   group('Sentry filter:', () {
     const remoteFile = 'Saber/a7c452ee393a9c995efce3cccd27bf7b.sbe';
-    const localFile = '/somenote.sbn2';
-    const syncFile = 'SaberSyncFile(local: $localFile, remote: $remoteFile)';
+    const localFile = '/path/to/somenote.sbn2';
 
     test('Redacts SaberSyncFile', () async {
       final originalEvent = SentryEvent(
@@ -14,7 +13,7 @@ void main() {
       final filteredEvent =
           await SentryFilter.beforeSend(originalEvent, Hint());
       expect(filteredEvent?.message?.formatted,
-          'Transfer complete: SaberSyncFile([redacted])');
+          'Uploaded: SaberSyncFile([redacted])');
     });
 
     test('Redacts remote file paths', () async {
@@ -24,6 +23,33 @@ void main() {
           await SentryFilter.beforeSend(originalEvent, Hint());
       expect(filteredEvent?.message?.formatted,
           'Remote file path: Saber/[redacted].sbe');
+    });
+
+    test('Redacts local file paths', () async {
+      final originalEvent =
+          SentryEvent(message: SentryMessage('Local file path: $localFile'));
+      final filteredEvent =
+          await SentryFilter.beforeSend(originalEvent, Hint());
+      expect(filteredEvent?.message?.formatted,
+          'Local file path: /path/to/[redacted].sbn2');
+    });
+
+    test('Redacts local asset paths', () async {
+      final originalEvent =
+          SentryEvent(message: SentryMessage('Asset: $localFile.0'));
+      final filteredEvent =
+          await SentryFilter.beforeSend(originalEvent, Hint());
+      expect(filteredEvent?.message?.formatted,
+          'Asset: /path/to/[redacted].sbn2.0');
+    });
+
+    test('Redacts domain names', () async {
+      final originalEvent = SentryEvent(
+          message: SentryMessage('Domain: https://example.com/index.php/#/'));
+      final filteredEvent =
+          await SentryFilter.beforeSend(originalEvent, Hint());
+      expect(filteredEvent?.message?.formatted,
+          'Domain: https://[redacted]/index.php/#/');
     });
   });
 }
