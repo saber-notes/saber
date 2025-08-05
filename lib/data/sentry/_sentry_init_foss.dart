072ce425619359dd157c61c7a6d386658f804eb7
Adil Hanney <adilhanney@disroot.org>
2025-08-06 00:53:51 +0100

    feat: opt-in to sentry analytics

--- lib/data/sentry/_sentry_init_foss.dart
+++ lib/data/sentry/_sentry_init_foss.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:saber/data/prefs.dart';
 
+const isSentryAvailable = false;
 const isSentryEnabled = false;
 
-Future<void> initSentry(FutureOr<void> Function() appRunner) async =>
-    appRunner();
+Future<void> initSentry(FutureOr<void> Function() appRunner) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Stows.markAsOnMainIsolate();
+  await stows.sentryConsent.waitUntilRead();
+
+  return appRunner();
+}
 
 class SentryWidget extends StatelessWidget {
   const SentryWidget({super.key, required this.child});
