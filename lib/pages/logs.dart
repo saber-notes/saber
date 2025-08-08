37caa6f2306c8806b45beedbb8a9ba96f9baac41
Adil Hanney <adilhanney@disroot.org>
2025-08-08 20:03:31 +0100

    test: logs page goldens

--- lib/pages/logs.dart
+++ lib/pages/logs.dart
@@ -32,6 +32,12 @@ class _LogsHistory extends ChangeNotifier {
     _history.add(record);
     notifyListeners();
   }
+
+  void clear() {
+    _history.clear();
+    _frozenHistory = null;
+    notifyListeners();
+  }
 }
 
 class LogsPage extends StatelessWidget {
@@ -40,8 +46,8 @@ class LogsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnimatedBuilder(
-        animation: logsHistory,
+      body: ListenableBuilder(
+        listenable: logsHistory,
         builder: (context, _) {
           final theme = Theme.of(context);
           final colorScheme = theme.colorScheme;
