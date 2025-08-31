730071dea6cba76a0f3bc8a70c21e667b591fffe
Adil Hanney <adilhanney@disroot.org>
2025-08-31 16:26:21 +0100

    test: all font weights

--- test/fonts_test.dart
+++ test/fonts_test.dart
@@ -13,32 +13,54 @@ const _appFonts = [
 ];
 
 void main() {
-  testGoldens('Fonts', (tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            children: [
-              for (final font in _appFonts)
+  group('Fonts', () {
+    for (final font in _appFonts) {
+      testGoldens(font, (tester) async {
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: Column(
+              children: [
                 Expanded(
-                  child: Text(
-                    font,
-                    style: TextStyle(fontFamily: font, fontSize: 48),
+                  child: FittedBox(
+                    child: Text(
+                      font,
+                      style: TextStyle(fontFamily: font, fontSize: 24),
+                    ),
                   ),
                 ),
-            ],
+                for (final fontWeight in FontWeight.values)
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        fontWeight.value.toString(),
+                        style:
+                            TextStyle(fontFamily: font, fontWeight: fontWeight),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        'The quick brown fox jumps over the lazy dog.\n'
+                        'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.\n'
+                        '0123456789',
+                        style:
+                            TextStyle(fontFamily: font, fontWeight: fontWeight),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
 
-    await tester.loadFonts(overriddenFonts: saberSansSerifFontFallbacks);
-    await tester.pumpAndSettle();
+        await tester.loadFonts(overriddenFonts: saberSansSerifFontFallbacks);
+        await tester.pumpAndSettle();
 
-    tester.useFuzzyComparator(allowedDiffPercent: 0.1);
-    await expectLater(
-      find.byType(Column),
-      matchesGoldenFile('goldens/fonts.png'),
-    );
+        tester.useFuzzyComparator(allowedDiffPercent: 0.1);
+        await expectLater(
+          find.byType(Column),
+          matchesGoldenFile('goldens/$font.png'),
+        );
+      });
+    }
   });
 }
