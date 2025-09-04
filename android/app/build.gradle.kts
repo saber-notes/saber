442118ce52f7573fc7817174590d29cd9418031b
Adil Hanney <adilhanney@disroot.org>
2025-09-04 22:49:15 +0100

    fix: update ndk to 28c to support 16KB page sizes
    NDK version r28 and higher compile 16 KB-aligned by default.
    https://developer.android.com/guide/practices/page-sizes#compile-16-kb-alignment

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.adilhanney.saber"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
