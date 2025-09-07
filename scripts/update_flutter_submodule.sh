fb518897a441dd7359dfdde306b095f2989165b0
Adil Hanney <adilhanney@disroot.org>
2025-09-07 19:02:20 +0100

    dev: autoupdate flutter version for linux builds

--- scripts/update_flutter_submodule.sh
+++ scripts/update_flutter_submodule.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# Update Flutter version used in F-Droid builds
 pushd submodules/flutter
 git fetch origin
 git reset --hard origin/stable
 git checkout stable
 git pull
 popd
+
+# Update Flutter version used in Linux builds
+FLUTTER_VERSION=$(flutter --version | head -n 1 | awk '{print $2}')
+sed -i -E "s/flutter-version: .*/flutter-version: $FLUTTER_VERSION/" .github/workflows/linux.yml
