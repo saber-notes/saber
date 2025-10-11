c3f92ad938c707fb51b6d18b0b6cc1d81a8042c0
Adil Hanney <adilhanney@disroot.org>
2025-10-11 02:05:49 +0100

    chore: remove unused script

--- scripts/update_flutter_submodule.sh
+++ scripts/update_flutter_submodule.sh
@@ -1,17 +1,3 @@
 #!/usr/bin/env bash
 
-# Update local Flutter SDK
-flutter channel stable
-flutter upgrade
-echo
-
-# Update Flutter submodule
-pushd submodules/flutter
-git fetch origin
-git checkout stable
-git reset --hard origin/stable
-git pull
-popd
-
-# Save the current Flutter version to a file
-cp $(dirname $(which flutter))/cache/flutter.version.json submodules/.flutter.version.json
+git submodule update -f --remote submodules/flutter
