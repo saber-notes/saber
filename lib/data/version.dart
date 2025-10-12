9dcf300315a4b651d0fd623f3371e0c817253049
Adil Hanney <adilhanney@disroot.org>
2025-10-12 03:22:25 +0100

    v0.26.9

--- lib/data/version.dart
+++ lib/data/version.dart
@@ -2,10 +2,10 @@
 // Run `./scripts/bump_version.dart --help` for more information.
 
 /// The current app version as an ordinal number.
-const int buildNumber = 26080;
+const int buildNumber = 26090;
 
 /// The current app version as a string.
-const String buildName = '0.26.8';
+const String buildName = '0.26.9';
 
 /// The year in which the current version was released.
 const int buildYear = 2025;
