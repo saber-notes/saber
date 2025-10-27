2d6eb32ea9386a34531093dd154102ac943b3534
Adil Hanney <adilhanney@disroot.org>
2025-10-27 12:20:39 +0000

    chore: remove dead code for previews

--- lib/components/canvas/canvas_preview.dart
+++ lib/components/canvas/canvas_preview.dart
@@ -1,13 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:saber/components/canvas/_stroke.dart';
 import 'package:saber/components/canvas/inner_canvas.dart';
 import 'package:saber/data/editor/editor_core_info.dart';
 import 'package:saber/data/editor/page.dart';
 import 'package:saber/data/extensions/list_extensions.dart';
-
-typedef _CacheItem = (EditorCoreInfo coreInfo, double pageHeight);
 
 class CanvasPreview extends StatelessWidget implements PreferredSizeWidget {
   CanvasPreview({
@@ -29,38 +25,6 @@ class CanvasPreview extends StatelessWidget implements PreferredSizeWidget {
       coreInfo.pages.getOrNull(pageIndex)?.size ?? EditorPage.defaultSize;
   @override
   late final preferredSize = Size(pageSize.width, height ?? pageSize.height);
-
-  static final _previewCache = <String, FutureOr<_CacheItem>>{};
-  static Widget fromFile({Key? key, required String filePath}) {
-    final future = _previewCache.putIfAbsent(filePath, () async {
-      final coreInfo = await EditorCoreInfo.loadFromFilePath(filePath);
-      final pageHeight = coreInfo.pages.isNotEmpty
-          ? coreInfo.pages[0].previewHeight(lineHeight: coreInfo.lineHeight)
-          : EditorPage.defaultHeight * 0.1;
-      return (coreInfo, pageHeight);
-    });
-
-    if (future is _CacheItem) {
-      // Don't use FutureBuilder which first builds with null
-      return CanvasPreview(key: key, coreInfo: future.$1, height: future.$2);
-    }
-
-    return FutureBuilder(
-      key: key,
-      future: future,
-      builder: (context, snapshot) {
-        final data = snapshot.data;
-        if (data == null) return const Icon(Icons.note, size: 48);
-
-        if (_previewCache[filePath] is Future<_CacheItem>) {
-          // Store result of future to make rebuilds faster
-          _previewCache[filePath] = data;
-        }
-
-        return CanvasPreview(key: key, coreInfo: data.$1, height: data.$2);
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
