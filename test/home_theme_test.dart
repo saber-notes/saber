c938b58feba2f06f521c275251adcf2d9c7de01e
Adil Hanney <adilhanney@disroot.org>
2025-11-09 17:22:29 +0000

    test: all navbar variants

---
+++ test/home_theme_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_screenshot/golden_screenshot.dart';
+import 'package:saber/components/theming/saber_theme.dart';
+import 'package:saber/data/flavor_config.dart';
+import 'package:saber/data/prefs.dart';
+import 'package:saber/data/sentry/sentry_consent.dart';
+import 'package:saber/pages/home/home.dart';
+
+void main() {
+  group('Home themes', () {
+    for (final platform in TargetPlatform.values)
+      for (final brightness in Brightness.values)
+        testGoldens('${platform.name}_${brightness.name}', (tester) async {
+          FlavorConfig.setup();
+          stows.sentryConsent.value = SentryConsent.granted;
+
+          final theme = SaberTheme.createThemeFromSeed(
+            Colors.yellow,
+            brightness,
+            platform,
+          );
+          await tester.pumpWidget(
+            ScreenshotApp.withConditionalTitlebar(
+              device: GoldenScreenshotDevices.androidPhone.device,
+              title: 'Saber',
+              theme: theme,
+              home: Theme(
+                data: theme,
+                child: const HomePage(
+                  subpage: HomePage.recentSubpage,
+                  path: '',
+                ),
+              ),
+            ),
+          );
+          await tester.loadAssets();
+          await tester.pumpAndSettle();
+
+          await tester.expectScreenshot(
+            GoldenScreenshotDevices.androidPhone.device,
+            'goldens/home_theme_${platform.name}_${brightness.name}',
+          );
+        });
+  });
+}
