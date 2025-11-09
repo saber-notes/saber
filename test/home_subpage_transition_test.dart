35ba5ddfaade326d4354da8b5919973c6de873ce
Adil Hanney <adilhanney@disroot.org>
2025-11-09 20:44:09 +0000

    test: dark flash when transitioning pages on mobile

--- test/home_subpage_transition_test.dart
+++ test/home_subpage_transition_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:go_router/go_router.dart';
 import 'package:golden_screenshot/golden_screenshot.dart';
+import 'package:saber/components/navbar/responsive_navbar.dart';
 import 'package:saber/components/theming/dynamic_material_app.dart';
 import 'package:saber/components/theming/saber_theme.dart';
 import 'package:saber/data/flavor_config.dart';
@@ -15,56 +16,62 @@ import 'package:saber/pages/home/home.dart';
 void main() {
   group('Home subpage transition', () {
     for (final brightness in Brightness.values)
-      testGoldens(brightness.name, (tester) async {
-        FlavorConfig.setup();
-        stows.sentryConsent.value = SentryConsent.granted;
+      for (final mobile in [true, false])
+        testGoldens(brightness.name + (mobile ? 'phone' : 'tablet'), (
+          tester,
+        ) async {
+          FlavorConfig.setup();
+          stows.sentryConsent.value = SentryConsent.granted;
+          stows.layoutSize.value = mobile
+              ? LayoutSize.phone
+              : LayoutSize.tablet;
 
-        final theme = SaberTheme.createThemeFromSeed(
-          Colors.yellow,
-          brightness,
-          TargetPlatform.android,
-        );
-        final router = GoRouter(
-          initialLocation: App.initialLocation,
-          routes: <GoRoute>[
-            GoRoute(
-              path: '/',
-              redirect: (context, state) => App.initialLocation,
-            ),
-            GoRoute(
-              path: RoutePaths.home,
-              builder: (context, state) => HomePage(
-                subpage:
-                    state.pathParameters['subpage'] ?? HomePage.recentSubpage,
-                path: state.uri.queryParameters['path'],
+          final theme = SaberTheme.createThemeFromSeed(
+            Colors.yellow,
+            brightness,
+            TargetPlatform.android,
+          );
+          final router = GoRouter(
+            initialLocation: App.initialLocation,
+            routes: <GoRoute>[
+              GoRoute(
+                path: '/',
+                redirect: (context, state) => App.initialLocation,
+              ),
+              GoRoute(
+                path: RoutePaths.home,
+                builder: (context, state) => HomePage(
+                  subpage:
+                      state.pathParameters['subpage'] ?? HomePage.recentSubpage,
+                  path: state.uri.queryParameters['path'],
+                ),
+              ),
+            ],
+          );
+          await tester.pumpWidget(
+            TranslationProvider(
+              child: ExplicitlyThemedApp(
+                title: 'Saber',
+                router: router,
+                themeMode: ThemeMode.light,
+                theme: theme,
+                darkTheme: theme,
               ),
             ),
-          ],
-        );
-        await tester.pumpWidget(
-          TranslationProvider(
-            child: ExplicitlyThemedApp(
-              title: 'Saber',
-              router: router,
-              themeMode: ThemeMode.light,
-              theme: theme,
-              darkTheme: theme,
+          );
+          await tester.loadAssets();
+          await tester.pumpAndSettle();
+
+          await tester.tap(find.byIcon(Icons.settings));
+          await tester.pump();
+          await tester.pump(const Duration(milliseconds: 150));
+
+          await expectLater(
+            find.byType(MaterialApp),
+            matchesGoldenFile(
+              'goldens/home_subpage_transition_${brightness.name}_${mobile ? 'phone' : 'tablet'}.png',
             ),
-          ),
-        );
-        await tester.loadAssets();
-        await tester.pumpAndSettle();
-
-        await tester.tap(find.byIcon(Icons.settings));
-        await tester.pump();
-        await tester.pump(const Duration(milliseconds: 150));
-
-        await expectLater(
-          find.byType(MaterialApp),
-          matchesGoldenFile(
-            'goldens/home_subpage_transition_${brightness.name}.png',
-          ),
-        );
-      });
+          );
+        });
   });
 }
