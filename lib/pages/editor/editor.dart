426ffe67ebaaad5b544d027eeebd5b791efba0ee
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2025-11-19 16:50:26 +0100

    fix: stylus button wrong tool when coming close again (#1457)
    Fix toggling back to wrong tool when selecting a tool while eraser was selected with pen button

--- lib/pages/editor/editor.dart
+++ lib/pages/editor/editor.dart
@@ -752,7 +752,7 @@ class EditorState extends State<Editor> {
         currentTool = Eraser();
         setState(() {});
       } else {
-        if (tmpTool != null) {
+        if (tmpTool != null && currentTool is Eraser) {
           currentTool = tmpTool!;
           tmpTool = null;
           setState(() {});
