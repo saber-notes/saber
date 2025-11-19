df209ee9acdbe6dc2db4e8db4f8873f81c1e0680
Adil Hanney <adilhanney@disroot.org>
2025-11-19 00:12:15 +0000

    ref: migrate ios to UISceneDelegate
    https://docs.flutter.dev/release/breaking-changes/uiscenedelegate#migration-guide-for-flutter-apps

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,9 +1,9 @@
-import UIKit
 import Flutter
+import UIKit
 import workmanager_apple
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   /// Registers all pubspec-referenced Flutter plugins in the given registry
   static func registerPlugins(with registry: FlutterPluginRegistry) {
     GeneratedPluginRegistrant.register(with: registry)
@@ -13,9 +13,6 @@ import workmanager_apple
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // register pubspec plugins in the context of a normal run
-    AppDelegate.registerPlugins(with: self)
-    
     WorkmanagerPlugin.setPluginRegistrantCallback { registry in
       // The following code will be called upon WorkmanagerPlugin's registration.
       AppDelegate.registerPlugins(with: registry)
@@ -25,5 +22,9 @@ import workmanager_apple
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(12 * 60 * 60))
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    AppDelegate.registerPlugins(with: engineBridge.pluginRegistry)
   }
 }
