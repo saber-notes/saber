fb0730c75853ce24fae478bd3e5fe1c6dcaf1af4
Adil Hanney <adilhanney@disroot.org>
2026-02-16 14:10:39 +0000

    v1.31.1

--- lib/data/version.dart
+++ lib/data/version.dart
@@ -2,10 +2,10 @@
 // Run `./scripts/bump_version.dart --help` for more information.
 
 /// The current app version as an ordinal number.
-const buildNumber = 131000;
+const buildNumber = 131010;
 
 /// The current app version as a string.
-const buildName = '1.31.0';
+const buildName = '1.31.1';
 
 /// The year in which the current version was released.
 const buildYear = 2026;
