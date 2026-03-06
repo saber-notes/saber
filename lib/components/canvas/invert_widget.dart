1dec24a5110888c92e722a656255703accc3a2e5
Adil Hanney <adilhanney@disroot.org>
2026-03-06 15:47:25 +0000

    ref: add an extra decimal point to invert matrix

--- lib/components/canvas/invert_widget.dart
+++ lib/components/canvas/invert_widget.dart
@@ -13,26 +13,18 @@ class InvertWidget extends StatelessWidget {
   }
 
   static const _invertFilter = ColorFilter.matrix(_invertMatrix);
+  // dart format off
   static const _invertMatrix = <double>[
-    1 - 2 * 0.213,
-    -2 * 0.715,
-    -2 * 0.072,
-    0,
-    255,
-    -2 * 0.213,
-    1 - 2 * 0.715,
-    -2 * 0.072,
-    0,
-    255,
-    -2 * 0.213,
-    -2 * 0.715,
-    1 - 2 * 0.072,
-    0,
-    255,
-    0,
-    0,
-    0,
-    1,
-    0,
+    1 - 2 * lumaR,   - 2 * lumaG,   - 2 * lumaB, 0, 255
+      - 2 * lumaR, 1 - 2 * lumaG,   - 2 * lumaB, 0, 255,
+      - 2 * lumaR,   - 2 * lumaG, 1 - 2 * lumaB, 0, 255,
+                0,             0,             0, 1,   0,
   ];
+  // dart format on
+
+  // Rec. 709 luma coefficients:
+  // https://en.wikipedia.org/wiki/Luma_%28video%29
+  static const lumaR = 0.2126;
+  static const lumaG = 0.7152;
+  static const lumaB = 0.0722;
 }
