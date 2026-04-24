53939dc6a7a200d61cb320cb6cb29d8f67ee27df
Aarron Lee <aarron-lee@users.noreply.github.com>
2026-04-24 10:44:22 -0500

    replace stubbed custom dir selector for foss build

--- patches/pre/enable_custom_dir_foss_android.sh
+++ patches/pre/enable_custom_dir_foss_android.sh
@@ -2,6 +2,8 @@
 #
 # Enables custom directories for the Android FOSS build
 
+# Update Manifest
+
 MANIFEST="android/app/src/main/AndroidManifest.xml"
 
 echo -n "Adding MANAGE_EXTERNAL_STORAGE permission to AndroidManifest.xml: "
@@ -12,3 +14,15 @@ else
   sed -i 's|<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />|<uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />\n    <uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" />|' "$MANIFEST"
 fi
 
+# replace stub with FOSS version of custom directory selector
+
+ANDROID_DART="lib/components/settings/custom_directory_selector_android.dart"
+FOSS_DART="lib/components/settings/custom_directory_selector_foss.dart"
+
+echo -n "Replacing custom_directory_selector_android.dart with FOSS version: "
+if diff -q "$ANDROID_DART" "$FOSS_DART" > /dev/null 2>&1; then
+  echo "already done"
+else
+  echo "replacing"
+  mv "$FOSS_DART" "$ANDROID_DART"
+fi
