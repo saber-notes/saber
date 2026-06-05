17f045d33e0fc7b6a4cfdb098ebac7c2a080a0a0
SandipLow <sandiplow.official@gmail.com>
2026-06-06 00:04:26 +0530

    Adding copy paste feature for the editor on selected strokes and Images
    # Conflicts:
    #	lib/i18n/strings_ar.g.dart
    #	lib/i18n/strings_ca.g.dart
    #	lib/i18n/strings_cs.g.dart
    #	lib/i18n/strings_de.g.dart
    #	lib/i18n/strings_en.g.dart
    #	lib/i18n/strings_eo.g.dart
    #	lib/i18n/strings_es.g.dart
    #	lib/i18n/strings_fa.g.dart
    #	lib/i18n/strings_fr.g.dart
    #	lib/i18n/strings_he.g.dart
    #	lib/i18n/strings_hu.g.dart
    #	lib/i18n/strings_it.g.dart
    #	lib/i18n/strings_ja.g.dart
    #	lib/i18n/strings_pl.g.dart
    #	lib/i18n/strings_pt_BR.g.dart
    #	lib/i18n/strings_ru.g.dart
    #	lib/i18n/strings_sl.g.dart
    #	lib/i18n/strings_th.g.dart
    #	lib/i18n/strings_tr.g.dart
    #	lib/i18n/strings_uk.g.dart
    #	lib/i18n/strings_vi.g.dart
    #	lib/i18n/strings_zh_Hans_CN.g.dart
    #	lib/i18n/strings_zh_Hant_TW.g.dart

--- lib/components/toolbar/selection_bar.dart
+++ lib/components/toolbar/selection_bar.dart
@@ -4,44 +4,79 @@ import 'package:saber/components/theming/adaptive_icon.dart';
 import 'package:saber/i18n/strings.g.dart';
 
 class SelectionBar extends StatelessWidget {
+  final VoidCallback copySelection;
   final VoidCallback duplicateSelection;
   final VoidCallback deleteSelection;
+  final VoidCallback paste;
+  final bool hasSelection;
 
   const SelectionBar({
     super.key,
+    required this.copySelection,
     required this.duplicateSelection,
     required this.deleteSelection,
+    required this.paste,
+    required this.hasSelection,
   });
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = ColorScheme.of(context);
     return Row(
-      mainAxisAlignment: .center,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        if (hasSelection) ...[
+          IconButton(
+            onPressed: copySelection,
+            style: TextButton.styleFrom(
+              foregroundColor: colorScheme.secondary,
+              backgroundColor: Colors.transparent,
+              shape: const CircleBorder(),
+            ),
+            tooltip: t.editor.selectionBar.copy,
+            icon: const AdaptiveIcon(
+              icon: Icons.content_copy,
+              cupertinoIcon: CupertinoIcons.doc_on_clipboard,
+            ),
+          ),
+          IconButton(
+            onPressed: duplicateSelection,
+            style: TextButton.styleFrom(
+              foregroundColor: colorScheme.secondary,
+              backgroundColor: Colors.transparent,
+              shape: const CircleBorder(),
+            ),
+            tooltip: t.editor.selectionBar.duplicate,
+            icon: const AdaptiveIcon(
+              icon: Icons.control_point_duplicate,
+              cupertinoIcon: CupertinoIcons.plus_square_on_square,
+            ),
+          ),
+          IconButton(
+            onPressed: deleteSelection,
+            style: TextButton.styleFrom(
+              foregroundColor: colorScheme.secondary,
+              backgroundColor: Colors.transparent,
+              shape: const CircleBorder(),
+            ),
+            tooltip: t.editor.selectionBar.delete,
+            icon: const AdaptiveIcon(
+              icon: Icons.delete,
+              cupertinoIcon: CupertinoIcons.delete,
+            ),
+          ),
+        ],
         IconButton(
-          onPressed: duplicateSelection,
+          onPressed: paste,
           style: TextButton.styleFrom(
-            foregroundColor: ColorScheme.of(context).secondary,
+            foregroundColor: colorScheme.secondary,
             backgroundColor: Colors.transparent,
             shape: const CircleBorder(),
           ),
-          tooltip: t.editor.selectionBar.duplicate,
+          tooltip: t.editor.selectionBar.paste,
           icon: const AdaptiveIcon(
-            icon: Icons.content_copy,
-            cupertinoIcon: CupertinoIcons.doc_on_clipboard,
-          ),
-        ),
-        IconButton(
-          onPressed: deleteSelection,
-          style: TextButton.styleFrom(
-            foregroundColor: ColorScheme.of(context).secondary,
-            backgroundColor: Colors.transparent,
-            shape: const CircleBorder(),
-          ),
-          tooltip: t.editor.selectionBar.delete,
-          icon: const AdaptiveIcon(
-            icon: Icons.delete,
-            cupertinoIcon: CupertinoIcons.delete,
+            icon: Icons.content_paste,
+            cupertinoIcon: CupertinoIcons.doc_on_clipboard_fill,
           ),
         ),
       ],
