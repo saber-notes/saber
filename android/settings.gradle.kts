828b20c35a6a5f6c3c863bb9bf0ac85c0399fa7a
Adil Hanney <adilhanney@disroot.org>
2026-08-18 03:05:18 +0100

    chore(android): migrate to gradle daemon toolchain
    https://docs.gradle.org/current/userguide/gradle_daemon.html

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,6 +20,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.3.1" apply false
     id("org.jetbrains.kotlin.android") version "2.4.10" apply false
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 include(":app")
