ddf7467aa9b9c35859f058d72e6673017bb6cf57
Adil Hanney <adilhanney@disroot.org>
2026-08-18 02:39:20 +0100

    chore(android): update to gradle 9

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,8 +18,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.12.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
+    id("com.android.application") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.10" apply false
 }
 
 include(":app")
