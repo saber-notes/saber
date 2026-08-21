aac7235ee06f0d065d43b19441f7bc0b3edeb3d6
Adil Hanney <adilhanney@disroot.org>
2026-08-21 17:31:48 +0100

    ci: delete remove_dev_dependencies.sh patch
    No longer needed with newer Flutter versions. dev_dependencies' assets are no longer included in release builds.

--- patches/pre/remove_dev_dependencies.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Removes development dependencies from pubspec.yaml
-# which are not needed for production
-
-set -e
-
-echo "Removing golden_screenshot from pubspec.yaml"
-sed -i -e "/golden_screenshot/d" pubspec.yaml
