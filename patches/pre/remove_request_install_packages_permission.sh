8205c04b9f7eee33e567f6e31655bc60e6009d19
Adil Hanney <adilhanney@disroot.org>
2026-08-21 17:55:55 +0100

    ci: delete `remove_request_install_packages_permission.sh` patch
    Hasn't done anything for a while

--- patches/pre/remove_request_install_packages_permission.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-echo "Warning: The remove_request_install_packages_permission patch is no longer necessary."
-echo "         Please remove it from your CI workflow."
-read -p "         Press Enter to acknowledge, or wait 10 seconds..." -t 10
